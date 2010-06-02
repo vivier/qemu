c52941ec4c6e9ef26cc47c141de23d70d3244f4d
Eduardo Habkost <ehabkost@redhat.com>
2010-06-01 22:26:04 -0300

    monitor: allow device to be ejected if no disk is inserted
    RH-Author: Eduardo Habkost <ehabkost@redhat.com>
    Message-id: <20100601222604.GE14354@blackpad.lan.raisama.net>
    Patchwork-id: 9657
    O-Subject: [RHEL-6 qemu-kvm PATCH] monitor: allow device to be ejected if no
    	disk is inserted
    Bugzilla: 581789
    RH-Acked-by: Alex Williamson <alex.williamson@redhat.com>
    RH-Acked-by: Markus Armbruster <armbru@redhat.com>
    RH-Acked-by: Juan Quintela <quintela@redhat.com>
    
    Bugzilla: 581789
    Upstream status: submitted
      (v3 submitted today. the previous submission was silently dropped from
      the staging tree in December)
    
    This changes the monitor eject_device() function to not check for
    bdrv_is_inserted().
    
    Example run where the bug manifests itself:
    
    (output of 'info block' is stripped to include only the CD-ROM device)
    
      (qemu) info block
      ide1-cd0: type=cdrom removable=1 locked=0 [not inserted]
      (qemu) change ide1-cd0 /dev/cdrom host_cdrom
      (qemu) info block
      ide1-cd0: type=cdrom removable=1 locked=0 file=/dev/cdrom ro=1 drv=host_cdrom encrypted=0
      (qemu) eject ide1-cd0
      (qemu) info block
      ide1-cd0: type=cdrom removable=1 locked=0 file=/dev/cdrom ro=1 drv=host_cdrom encrypted=0
    
      # at this point, a disk was inserted on the host CD-ROM drive
    
      (qemu) info block
      ide1-cd0: type=cdrom removable=1 locked=0 file=/dev/cdrom ro=1 drv=host_cdrom encrypted=0
      (qemu) eject ide1-cd0
      (qemu) info block
      ide1-cd0: type=cdrom removable=1 locked=0 [not inserted]
      (qemu)
    
    The first eject command didn't work because the is_inserted() check
    failed.
    
    I have no clue why the code had the is_inserted() check, as it doesn't matter
    if there is a disk present at the host drive, when the user wants the virtual
    device to be disconnected from the host device.
    
    The is_inserted() check has another side effect: a memory leak if the "change"
    command is used multiple times, as do_change() calls eject_device() before
    re-opening the block device, but bdrv_close() is never called.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    ---
     monitor.c |   22 ++++++++++------------
     1 files changed, 10 insertions(+), 12 deletions(-)
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- monitor.c
+++ monitor.c
@@ -1062,20 +1062,18 @@ static int do_quit(Monitor *mon, const QDict *qdict, QObject **ret_data)
 
 static int eject_device(Monitor *mon, BlockDriverState *bs, int force)
 {
-    if (bdrv_is_inserted(bs)) {
-        if (!force) {
-            if (!bdrv_is_removable(bs)) {
-                qerror_report(QERR_DEVICE_NOT_REMOVABLE,
-                               bdrv_get_device_name(bs));
-                return -1;
-            }
-            if (bdrv_is_locked(bs)) {
-                qerror_report(QERR_DEVICE_LOCKED, bdrv_get_device_name(bs));
-                return -1;
-            }
+    if (!force) {
+        if (!bdrv_is_removable(bs)) {
+            qerror_report(QERR_DEVICE_NOT_REMOVABLE,
+                           bdrv_get_device_name(bs));
+            return -1;
         }
-        bdrv_close(bs);
+        if (bdrv_is_locked(bs)) {
+            qerror_report(QERR_DEVICE_LOCKED, bdrv_get_device_name(bs));
+            return -1;
+        }
     }
+    bdrv_close(bs);
     return 0;
 }
 
