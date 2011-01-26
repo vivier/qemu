77103d63ac3d6c292035bee983cf60672399343d
Anthony Liguori <aliguori@redhat.com>
2011-01-26 14:57:56 -0200

    ide: Remove redundant IDEState member conf
    RH-Author: Anthony Liguori <aliguori@redhat.com>
    Message-id: <1296053886-2905-2-git-send-email-aliguori@redhat.com>
    Patchwork-id: 17088
    O-Subject: [PATCH RHEL6.1 qemu-kvm 01/11] ide: Remove redundant IDEState member
    	conf
    Bugzilla: 654682
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    RH-Acked-by: Marcelo Tosatti <mtosatti@redhat.com>
    RH-Acked-by: Markus Armbruster <armbru@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    From: Markus Armbruster <armbru@redhat.com>
    
    BZ: 654682
    Upstream-status: accepted
    
    Commit 428c149b added IDEState member conf to let commit 0009baf1 find
    the BlockConf from there.  It exists only for qdev drives, created via
    ide_drive_initfn(), not for drives created via ide_init2().
    
    But for a qdev drive, we can just as well reach its IDEDevice, which
    contains the BlockConf.  Do that, and revert the parts of commit
    428c149b that add IDEState member conf.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit 57dac7ef8aeabcd17b1ceee2296917bc48ccd526)
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- hw/ide/qdev.c
+++ hw/ide/qdev.c
@@ -113,8 +113,7 @@ typedef struct IDEDrive {
 static int ide_drive_initfn(IDEDevice *dev)
 {
     IDEBus *bus = DO_UPCAST(IDEBus, qbus, dev->qdev.parent_bus);
-    ide_init_drive(bus->ifs + dev->unit, dev->conf.dinfo, &dev->conf,
-                   dev->version);
+    ide_init_drive(bus->ifs + dev->unit, dev->conf.dinfo, dev->version);
     add_boot_device_path(dev->conf.bootindex, &dev->qdev,
                          dev->unit ? "/disk@1" : "/disk@0");
 
