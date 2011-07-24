6bfd1c072e29eda1a441d4a32bf03d47fefb2485
Alon Levy <alevy@redhat.com>
2011-07-24 16:34:23 -0300

    qxl: async io support using new spice api
    RH-Author: Alon Levy <alevy@redhat.com>
    Message-id: <1311525266-12705-14-git-send-email-alevy@redhat.com>
    Patchwork-id: 30255
    O-Subject: [PATCH RHEL6.2 13/16] qxl: async io support using new spice api
    Bugzilla: 700134
    RH-Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    RH-Acked-by: Yonit Halperin <yhalperi@redhat.com>
    RH-Acked-by: Arnon Gilboa <agilboa@redhat.com>
    
    Some of the QXL port i/o commands are waiting for the spice server to
    complete certain actions.  Add async versions for these commands, so we
    don't block the vcpu while the spice server processses the command.
    Instead the qxl device will raise an IRQ when done.
    
    The async command processing relies on an added QXLInterface::async_complete
    and added QXLWorker::*_async additions, in spice server qxl >= 3.1
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Alon Levy     <alevy@redhat.com>
    
    upstream: http://patchwork.ozlabs.org/patch/105617/
     acked, slated for 0.16.0
    
    Conflicts (because of rhel only qxl_screendump):
    
    	hw/qxl.h
    ---
     hw/qxl-render.c    |    2 +-
     hw/qxl.c           |  240 ++++++++++++++++++++++++++++++++++++++++++++--------
     hw/qxl.h           |   16 +++-
     ui/spice-display.c |   47 ++++++++--
     ui/spice-display.h |   23 +++++-
     5 files changed, 274 insertions(+), 54 deletions(-)
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- ui/spice-display.c
+++ ui/spice-display.c
@@ -63,10 +63,18 @@ void qemu_spice_rect_union(QXLRect *dest, const QXLRect *r)
     dest->right = MAX(dest->right, r->right);
 }
 
-
-void qemu_spice_add_memslot(SimpleSpiceDisplay *ssd, QXLDevMemSlot *memslot)
+void qemu_spice_add_memslot(SimpleSpiceDisplay *ssd, QXLDevMemSlot *memslot,
+                            qxl_async_io async)
 {
-    ssd->worker->add_memslot(ssd->worker, memslot);
+    if (async != QXL_SYNC) {
+#if SPICE_INTERFACE_QXL_MINOR >= 1
+        spice_qxl_add_memslot_async(&ssd->qxl, memslot, 0);
+#else
+        abort();
+#endif
+    } else {
+        ssd->worker->add_memslot(ssd->worker, memslot);
+    }
 }
 
 void qemu_spice_del_memslot(SimpleSpiceDisplay *ssd, uint32_t gid, uint32_t sid)
@@ -75,14 +83,33 @@ void qemu_spice_del_memslot(SimpleSpiceDisplay *ssd, uint32_t gid, uint32_t sid)
 }
 
 void qemu_spice_create_primary_surface(SimpleSpiceDisplay *ssd, uint32_t id,
-                                       QXLDevSurfaceCreate *surface)
+                                       QXLDevSurfaceCreate *surface,
+                                       qxl_async_io async)
 {
-    ssd->worker->create_primary_surface(ssd->worker, id, surface);
+    if (async != QXL_SYNC) {
+#if SPICE_INTERFACE_QXL_MINOR >= 1
+        spice_qxl_create_primary_surface_async(&ssd->qxl, id, surface, 0);
+#else
+        abort();
+#endif
+    } else {
+        ssd->worker->create_primary_surface(ssd->worker, id, surface);
+    }
 }
 
-void qemu_spice_destroy_primary_surface(SimpleSpiceDisplay *ssd, uint32_t id)
+
+void qemu_spice_destroy_primary_surface(SimpleSpiceDisplay *ssd,
+                                        uint32_t id, qxl_async_io async)
 {
-    ssd->worker->destroy_primary_surface(ssd->worker, id);
+    if (async != QXL_SYNC) {
+#if SPICE_INTERFACE_QXL_MINOR >= 1
+        spice_qxl_destroy_primary_surface_async(&ssd->qxl, id, 0);
+#else
+        abort();
+#endif
+    } else {
+        ssd->worker->destroy_primary_surface(ssd->worker, id);
+    }
 }
 
 void qemu_spice_wakeup(SimpleSpiceDisplay *ssd)
@@ -194,7 +221,7 @@ void qemu_spice_create_host_memslot(SimpleSpiceDisplay *ssd)
     memset(&memslot, 0, sizeof(memslot));
     memslot.slot_group_id = MEMSLOT_GROUP_HOST;
     memslot.virt_end = ~0;
-    qemu_spice_add_memslot(ssd, &memslot);
+    qemu_spice_add_memslot(ssd, &memslot, QXL_SYNC);
 }
 
 void qemu_spice_create_host_primary(SimpleSpiceDisplay *ssd)
@@ -214,14 +241,14 @@ void qemu_spice_create_host_primary(SimpleSpiceDisplay *ssd)
     surface.mem        = (intptr_t)ssd->buf;
     surface.group_id   = MEMSLOT_GROUP_HOST;
 
-    qemu_spice_create_primary_surface(ssd, 0, &surface);
+    qemu_spice_create_primary_surface(ssd, 0, &surface, QXL_SYNC);
 }
 
 void qemu_spice_destroy_host_primary(SimpleSpiceDisplay *ssd)
 {
     dprint(1, "%s:\n", __FUNCTION__);
 
-    qemu_spice_destroy_primary_surface(ssd, 0);
+    qemu_spice_destroy_primary_surface(ssd, 0, QXL_SYNC);
 }
 
 void qemu_spice_vm_change_state_handler(void *opaque, int running, int reason)
