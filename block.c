618fa3644ab3d95c570fb4d52e88c783884f2033
Markus Armbruster <armbru@redhat.com>
2011-10-04 16:24:37 +0200

    block: Revert entanglement of bdrv_is_inserted() with tray status
    RH-Author: Markus Armbruster <armbru@redhat.com>
    Message-id: <1317745491-18401-55-git-send-email-armbru@redhat.com>
    Patchwork-id: 33636
    O-Subject: [PATCH RHEL-6.2 qemu-kvm 54/68] block: Revert entanglement of bdrv_is_inserted() with tray status
    Bugzilla: 742458
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Amit Shah <amit.shah@redhat.com>
    RH-Acked-by: Juan Quintela <quintela@redhat.com>
    
    Commit 4be9762a changed bdrv_is_inserted() to fail when the tray is
    open.  Unfortunately, there are two different kinds of users, with
    conflicting needs.
    
    1. Device models using bdrv_eject(), currently ide-cd and scsi-cd.
    They expect bdrv_is_inserted() to reflect the tray status.  Commit
    4be9762a makes them happy.
    
    2. Code that wants to know whether a BlockDriverState has media, such
    as find_image_format(), bdrv_flush_all().  Commit 4be9762a makes them
    unhappy.  In particular, it breaks flush on VM stop for media ejected
    by the guest.
    
    Revert the change to bdrv_is_inserted().  Check the tray status in the
    device models instead.
    
    Note on IDE: Since only ATAPI devices have a tray, and they don't
    accept ATA commands since the recent commit "ide: Reject ATA commands
    specific to drive kinds", checking in atapi.c suffices.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit a1aff5bf6786e6e8478373e4ada869a4ef2a7fc4)
    ---
     block.c        |    7 +++----
     hw/ide/atapi.c |   15 ++++++++-------
     hw/scsi-disk.c |   10 ++++++++--
     3 files changed, 19 insertions(+), 13 deletions(-)
    
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- block.c
+++ block.c
@@ -2607,13 +2607,12 @@ void qemu_aio_release(void *p)
 int bdrv_is_inserted(BlockDriverState *bs)
 {
     BlockDriver *drv = bs->drv;
-    int ret;
+
     if (!drv)
         return 0;
     if (!drv->bdrv_is_inserted)
-        return !bs->tray_open;
-    ret = drv->bdrv_is_inserted(bs);
-    return ret;
+        return 1;
+    return drv->bdrv_is_inserted(bs);
 }
 
 /**
