e38fa9314a7eeaf5df0521f579fc0ebf207bc1cf
Kevin Wolf <kwolf@redhat.com>
2012-02-14 11:14:54 +0100

    block: drop redundant bdrv_flush implementation
    RH-Author: Kevin Wolf <kwolf@redhat.com>
    Message-id: <1329218101-24213-90-git-send-email-kwolf@redhat.com>
    Patchwork-id: 37280
    O-Subject: [RHEL-6.3 qemu-kvm PATCH v2 89/96] block: drop redundant bdrv_flush implementation
    Bugzilla: 783950
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Marcelo Tosatti <mtosatti@redhat.com>
    RH-Acked-by: Laszlo Ersek <lersek@redhat.com>
    
    From: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
    
    Bugzilla: 783950
    
    Block drivers now only need to provide either of .bdrv_co_flush,
    .bdrv_aio_flush() or for legacy drivers .bdrv_flush().  Remove
    the redundant .bdrv_flush() implementations.
    
    [Paolo Bonzini: change raw driver to bdrv_co_flush]
    
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit 6f6dc6565e2b65ec8c0cf47622b4f5f1468d194e)
    
    Conflicts:
    
    	block/blkverify.c
    	block/raw-posix.c
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    ---
     block/blkdebug.c  |    6 ------
     block/qcow.c      |    6 ------
     block/qcow2.c     |   19 -------------------
     block/qed.c       |    6 ------
     block/raw-posix.c |   18 ------------------
     block/raw.c       |   13 +++----------
     6 files changed, 3 insertions(+), 65 deletions(-)
    
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- block/raw-posix.c
+++ block/raw-posix.c
@@ -523,19 +523,6 @@ static int raw_create(const char *filename, QEMUOptionParameter *options)
     return result;
 }
 
-static int raw_flush(BlockDriverState *bs)
-{
-    BDRVRawState *s = bs->opaque;
-    int ret;
-
-    ret = qemu_fdatasync(s->fd);
-    if (ret < 0) {
-        return -errno;
-    }
-
-    return 0;
-}
-
 #ifdef CONFIG_XFS
 static int xfs_discard(BDRVRawState *s, int64_t sector_num, int nb_sectors)
 {
@@ -585,7 +572,6 @@ static BlockDriver bdrv_file = {
     .bdrv_file_open = raw_open,
     .bdrv_close = raw_close,
     .bdrv_create = raw_create,
-    .bdrv_flush = raw_flush,
     .bdrv_discard = raw_discard,
 
     .bdrv_aio_readv = raw_aio_readv,
@@ -848,7 +834,6 @@ static BlockDriver bdrv_host_device = {
     .bdrv_create        = hdev_create,
     .create_options     = raw_create_options,
     .bdrv_has_zero_init = hdev_has_zero_init,
-    .bdrv_flush         = raw_flush,
 
     .bdrv_aio_readv	= raw_aio_readv,
     .bdrv_aio_writev	= raw_aio_writev,
@@ -943,7 +928,6 @@ static BlockDriver bdrv_host_floppy = {
     .bdrv_create        = hdev_create,
     .create_options     = raw_create_options,
     .bdrv_has_zero_init = hdev_has_zero_init,
-    .bdrv_flush         = raw_flush,
 
     .bdrv_aio_readv     = raw_aio_readv,
     .bdrv_aio_writev    = raw_aio_writev,
@@ -1038,7 +1022,6 @@ static BlockDriver bdrv_host_cdrom = {
     .bdrv_create        = hdev_create,
     .create_options     = raw_create_options,
     .bdrv_has_zero_init = hdev_has_zero_init,
-    .bdrv_flush         = raw_flush,
 
     .bdrv_aio_readv     = raw_aio_readv,
     .bdrv_aio_writev    = raw_aio_writev,
@@ -1156,7 +1139,6 @@ static BlockDriver bdrv_host_cdrom = {
     .bdrv_create        = hdev_create,
     .create_options     = raw_create_options,
     .bdrv_has_zero_init = hdev_has_zero_init,
-    .bdrv_flush         = raw_flush,
 
     .bdrv_aio_readv     = raw_aio_readv,
     .bdrv_aio_writev    = raw_aio_writev,
