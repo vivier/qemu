36b50871c37adefda4c21758dc5af766459eca41
Paolo Bonzini <pbonzini@redhat.com>
2012-02-22 14:11:59 +0100

    scsi: notify the device when unit attention is reported
    RH-Author: Paolo Bonzini <pbonzini@redhat.com>
    Message-id: <1329919979-20948-43-git-send-email-pbonzini@redhat.com>
    Patchwork-id: 37526
    O-Subject: [RHEL 6.3 qemu-kvm PATCH v2 042/102] scsi: notify the device when unit attention is reported
    Bugzilla: 782029
    RH-Acked-by: Laszlo Ersek <lersek@redhat.com>
    RH-Acked-by: Orit Wasserman <owasserm@redhat.com>
    RH-Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    
    Reporting media change events via unit attention sense codes requires
    a small state machine: first report "NO MEDIUM", then report "MEDIUM MAY
    HAVE CHANGED".  Unfortunately there is no good hooking point for the
    device to notice that its pending unit attention condition has been
    reported.  This patch reworks the generic machinery to add one.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from 3653d8c40e04d78eb6b5b069522daf7cc72d4f06)
    ---
     hw/scsi-bus.c |   32 +++++++++++++++++++++++++++-----
     hw/scsi.h     |    2 ++
     2 files changed, 29 insertions(+), 5 deletions(-)
    
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- hw/scsi.h
+++ hw/scsi.h
@@ -64,6 +64,7 @@ struct SCSIDevice
     BlockConf conf;
     SCSIDeviceInfo *info;
     SCSISense unit_attention;
+    bool sense_is_ua;
     uint8_t sense[SCSI_SENSE_BUF_SIZE];
     uint32_t sense_len;
     QTAILQ_HEAD(, SCSIRequest) requests;
@@ -95,6 +96,7 @@ struct SCSIDeviceInfo {
     void (*destroy)(SCSIDevice *s);
     SCSIRequest *(*alloc_req)(SCSIDevice *s, uint32_t tag, uint32_t lun,
                               void *hba_private);
+    void (*unit_attention_reported)(SCSIDevice *s);
     SCSIReqOps reqops;
 };
 
