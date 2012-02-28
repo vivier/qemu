8b3b565ea533adcf930c3d2da3c78fb43032058a
Paolo Bonzini <pbonzini@redhat.com>
2012-02-28 12:53:14 +0100

    kvm: Flush coalesced MMIO buffer periodly
    RH-Author: Paolo Bonzini <pbonzini@redhat.com>
    Message-id: <1330433598-21534-2-git-send-email-pbonzini@redhat.com>
    Patchwork-id: 37668
    O-Subject: [RHEL 6.3 qemu-kvm PATCH 1/5] kvm: Flush coalesced MMIO buffer periodly
    Bugzilla: 796575
    RH-Acked-by: Alex Williamson <alex.williamson@redhat.com>
    RH-Acked-by: Laszlo Ersek <lersek@redhat.com>
    RH-Acked-by: Markus Armbruster <armbru@redhat.com>
    
    From: Sheng Yang <sheng@linux.intel.com>
    
    The default action of coalesced MMIO is, cache the writing in buffer, until:
    1. The buffer is full.
    2. Or the exit to QEmu due to other reasons.
    
    But this would result in a very late writing in some condition.
    1. The each time write to MMIO content is small.
    2. The writing interval is big.
    3. No need for input or accessing other devices frequently.
    
    This issue was observed in a experimental embbed system. The test image
    simply print "test" every 1 seconds. The output in QEmu meets expectation,
    but the output in KVM is delayed for seconds.
    
    Per Avi's suggestion, I hooked flushing coalesced MMIO buffer in VGA update
    handler. By this way, We don't need vcpu explicit exit to QEmu to
    handle this issue.
    
    Signed-off-by: Sheng Yang <sheng@linux.intel.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>
    (cherry picked from commit 62a2744ca09a0b44b8406ea0c430c4c67a2c3231)
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    ---
     cpu-all.h  |    2 ++
     exec.c     |    6 ++++++
     kvm-all.c  |   23 +++++++++++++++--------
     kvm.h      |    2 ++
     qemu-kvm.c |   37 ++++++++++++++++++++++++-------------
     qemu-kvm.h |    3 +++
     vl.c       |    2 ++
     7 files changed, 54 insertions(+), 21 deletions(-)
    
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- vl.c
+++ vl.c
@@ -3205,6 +3205,7 @@ static void gui_update(void *opaque)
     DisplayState *ds = opaque;
     DisplayChangeListener *dcl = ds->listeners;
 
+    qemu_flush_coalesced_mmio_buffer();
     dpy_refresh(ds);
 
     while (dcl != NULL) {
@@ -3220,6 +3221,7 @@ static void nographic_update(void *opaque)
 {
     uint64_t interval = GUI_REFRESH_INTERVAL;
 
+    qemu_flush_coalesced_mmio_buffer();
     qemu_mod_timer(nographic_timer, interval + qemu_get_clock(rt_clock));
 }
 
