02ae3b18a3b3dca16685224c9f6c6bd81edf8e41
Luiz Capitulino <lcapitulino@redhat.com>
2012-02-29 20:01:01 +0100

    block: Rename bdrv_mon_event() & BlockMonEventAction
    RH-Author: Luiz Capitulino <lcapitulino@redhat.com>
    Message-id: <1330545665-1595-2-git-send-email-lcapitulino@redhat.com>
    Patchwork-id: 37788
    O-Subject: [PATCH RHEL6.3 qemu-kvm 1/5] block: Rename bdrv_mon_event() & BlockMonEventAction
    Bugzilla: 575159
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Markus Armbruster <armbru@redhat.com>
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    
    This is a RHEL6 version of upstream commit:
    329c0a48a92664eb48b70993c0f2473b37aa7429.
    
    Cherry picking is just impossible, it conflicts in all files where
    the change is made. The solution is to re-do the change for RHEL6.
    
    Original changelog is:
    
        They are QMP events, not monitor events. Rename them accordingly.
    
        Also, move bdrv_emit_qmp_error_event() up in the file. A new event will
        be added soon and it's good to have them next each other.
    
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    ---
     block.c         |  142 +++++++++++++++++++++++++++---------------------------
     block.h         |    6 +-
     hw/ide/core.c   |    6 +-
     hw/scsi-disk.c  |    6 +-
     hw/virtio-blk.c |    6 +-
     5 files changed, 83 insertions(+), 83 deletions(-)
    
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- block.h
+++ block.h
@@ -85,7 +85,7 @@ typedef enum {
 
 typedef enum {
     BDRV_ACTION_REPORT, BDRV_ACTION_IGNORE, BDRV_ACTION_STOP
-} BlockMonEventAction;
+} BlockQMPEventAction;
 
 typedef enum {
     BDRV_IOS_INVAL, BDRV_IOS_OK, BDRV_IOS_FAILED, BDRV_IOS_ENOSPC,
@@ -97,8 +97,8 @@ void bdrv_iostatus_reset(BlockDriverState *bs);
 void bdrv_iostatus_disable(BlockDriverState *bs);
 bool bdrv_iostatus_is_enabled(const BlockDriverState *bs);
 void bdrv_iostatus_set_err(BlockDriverState *bs, int error);
-void bdrv_mon_event(const BlockDriverState *bdrv,
-                    BlockMonEventAction action, int error, int is_read);
+void bdrv_emit_qmp_error_event(const BlockDriverState *bdrv,
+                    BlockQMPEventAction action, int error, int is_read);
 void bdrv_info_print(Monitor *mon, const QObject *data);
 void bdrv_info(Monitor *mon, QObject **ret_data);
 void bdrv_stats_print(Monitor *mon, const QObject *data);
