5aee847dabaf137be7ac4f4b50e276b46f0ef50b
Luiz Capitulino <lcapitulino@redhat.com>
2012-03-05 17:43:35 -0500

    QError: Introduce QERR_QMP_EXTRA_MEMBER
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    (cherry picked from commit 60d76d7b073f6f25c9dbe4d68df7d012f73db27f)
    
    Conflicts:
    
    	qerror.c
    	qerror.h
    Signed-off-by: Jeff Cody <jcody@redhat.com>

--- qerror.h
+++ qerror.h
@@ -161,6 +161,9 @@ QError *qobject_to_qerror(const QObject *obj);
 #define QERR_QMP_BAD_INPUT_OBJECT_MEMBER \
     "{ 'class': 'QMPBadInputObjectMember', 'data': { 'member': %s, 'expected': %s } }"
 
+#define QERR_QMP_EXTRA_MEMBER \
+    "{ 'class': 'QMPExtraInputObjectMember', 'data': { 'member': %s } }"
+
 #define QERR_RESET_REQUIRED \
     "{ 'class': 'ResetRequired', 'data': {} }"
 
