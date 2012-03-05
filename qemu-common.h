8e01d98ab4f438392dd34acf604fb64a386f90bd
Luiz Capitulino <lcapitulino@gmail.com>
2012-03-05 17:43:37 -0500

    Introduce compiler.h header file
    This moves compiler related macros from qemu-common.h to compiler.h.
    
    The reason for this change is that there are simple header files that
    depend only on the compiler macros, so including qemu-common.h is overkill.
    
    Besides, qemu-common.h is bloated and will benefit from some splitting.
    
    Please, also note that the QEMU_BUILD_BUG_ON() macro is being fixed to
    not use double underscores as a prefix and the license text was added
    by Vassili Karpov (malc), who is one of the authors of the new file.
    
    Signed-off-by: Luiz Capitulino <lcapitulino@gmail.com>
    (cherry picked from commit 5c0263204da1870243107acd6c73b21f2e7b78f0)
    BZ 632771
    
    Conflicts:
    
    	qemu-common.h
    Signed-off-by: Jeff Cody <jcody@redhat.com>

--- qemu-common.h
+++ qemu-common.h
@@ -2,14 +2,8 @@
 #ifndef QEMU_COMMON_H
 #define QEMU_COMMON_H
 
+#include "compiler.h"
 #include "config-host.h"
-
-#define QEMU_NORETURN __attribute__ ((__noreturn__))
-#ifdef CONFIG_GCC_ATTRIBUTE_WARN_UNUSED_RESULT
-#define QEMU_WARN_UNUSED_RESULT __attribute__((warn_unused_result))
-#else
-#define QEMU_WARN_UNUSED_RESULT
-#endif
 
 typedef struct DeviceState DeviceState;
 
@@ -68,21 +62,8 @@ struct iovec {
 #include <sys/uio.h>
 #endif
 
-#if defined __GNUC__
-# if (__GNUC__ < 4) || \
-     defined(__GNUC_MINOR__) && (__GNUC__ == 4) && (__GNUC_MINOR__ < 4)
-   /* gcc versions before 4.4.x don't support gnu_printf, so use printf. */
-#  define GCC_ATTR __attribute__((__unused__, format(printf, 1, 2)))
-#  define GCC_FMT_ATTR(n, m) __attribute__((format(printf, n, m)))
-# else
-   /* Use gnu_printf when supported (qemu uses standard format strings). */
-#  define GCC_ATTR __attribute__((__unused__, format(gnu_printf, 1, 2)))
-#  define GCC_FMT_ATTR(n, m) __attribute__((format(gnu_printf, n, m)))
-# endif
-#else
-#define GCC_ATTR /**/
-#define GCC_FMT_ATTR(n, m)
-#endif
+typedef int (*fprintf_function)(FILE *f, const char *fmt, ...)
+    GCC_FMT_ATTR(2, 3);
 
 #ifdef _WIN32
 #define fsync _commit
