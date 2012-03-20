055fa61145def2954a19ad2199d5564ad8978e14
Jeffrey Cody <jcody@redhat.com>
2012-03-20 14:07:39 -0300

    qapi: Introduce blockdev-group-snapshot-sync command
    RH-Author: Jeffrey Cody <jcody@redhat.com>
    Message-id: <c1c5e2ac150cbd1c6c22c63acc2fc965e9a63005.1332249823.git.jcody@redhat.com>
    Patchwork-id: 38646
    O-Subject: [RHEL6.3 qemu-kvm PATCH v3 19/21] qapi: Introduce blockdev-group-snapshot-sync command
    Bugzilla: 784153
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Markus Armbruster <armbru@redhat.com>
    
    This is a QAPI/QMP only command to take a snapshot of a group of
    devices. This is similar to the blockdev-snapshot-sync command, except
    blockdev-group-snapshot-sync accepts a list devices, filenames, and
    formats.
    
    It is attempted to keep the snapshot of the group atomic; if the
    creation or open of any of the new snapshots fails, then all of
    the new snapshots are abandoned, and the name of the snapshot image
    that failed is returned.  The failure case should not interrupt
    any operations.
    
    Rather than use bdrv_close() along with a subsequent bdrv_open() to
    perform the pivot, the original image is never closed and the new
    image is placed 'in front' of the original image via manipulation
    of the BlockDriverState fields.  Thus, once the new snapshot image
    has been successfully created, there are no more failure points
    before pivoting to the new snapshot.
    
    This allows the group of disks to remain consistent with each other,
    even across snapshot failures.
    
    Signed-off-by: Jeff Cody <jcody@redhat.com>
    Acked-by: Luiz Capitulino <lcapitulino@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit 8802d1fdd4b73e02ce13fb3a233c64c1913634ab)
    
    Conflicts:
    
    	blockdev.c
    	qapi-schema.json
    
    RHEL6 Note: This is the only command in qapi-schema.json at this point,
                and changed bdrv_drain_all() to qemu_aio_flush()
    ---
     block.c          |   59 ++++++++++++++++++++++++
     block.h          |    1 +
     block_int.h      |    6 +++
     blockdev.c       |  131 ++++++++++++++++++++++++++++++++++++++++++++++++++++++
     qapi-schema.json |   38 ++++++++++++++++
     5 files changed, 235 insertions(+), 0 deletions(-)
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- block.h
+++ block.h
@@ -118,6 +118,7 @@ int bdrv_create2(BlockDriver *drv,
                  int flags);
 BlockDriverState *bdrv_new(const char *device_name);
 void bdrv_make_anon(BlockDriverState *bs);
+void bdrv_append(BlockDriverState *bs_new, BlockDriverState *bs_top);
 void bdrv_delete(BlockDriverState *bs);
 int bdrv_file_open(BlockDriverState **pbs, const char *filename, int flags);
 int bdrv_open(BlockDriverState *bs, const char *filename, int flags,
