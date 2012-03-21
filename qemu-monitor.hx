45c77a8122f65ebcd34fb4202b65704d3fc1aee9
Jeffrey Cody <jcody@redhat.com>
2012-03-21 21:54:29 +0100

    Revert "block: add -drive stream=on|off"
    RH-Author: Jeffrey Cody <jcody@redhat.com>
    Message-id: <d37fc03734cab84f43bfa989865d1939a239a36a.1332362400.git.jcody@redhat.com>
    Patchwork-id: 38852
    O-Subject: [RHEL6.3 qemu-kvm PATCH v8 02/54] Revert "block: add -drive stream=on|off"
    Bugzilla: 582475
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Marcelo Tosatti <mtosatti@redhat.com>
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    
    From: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
    
    This reverts commit c46b43d62c0f9384c719896217b94801e116300d.
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    Signed-off-by: Jeff Cody <jcody@redhat.com>
    ---
     blockdev.c      |   12 +-----------
     qemu-config.c   |    4 ----
     qemu-monitor.hx |    2 +-
     qemu-options.hx |    5 +----
     4 files changed, 3 insertions(+), 20 deletions(-)
    
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- qemu-monitor.hx
+++ qemu-monitor.hx
@@ -1465,7 +1465,7 @@ EQMP
                       "[,unit=m][,media=d][index=i]\n"
                       "[,cyls=c,heads=h,secs=s[,trans=t]]\n"
                       "[snapshot=on|off][,cache=on|off]\n"
-                      "[,copy-on-read=on|off][,stream=on|off]",
+                      "[,copy-on-read=on|off]",
         .help       = "add drive to PCI storage controller",
         .mhandler.cmd = drive_hot_add,
     },
