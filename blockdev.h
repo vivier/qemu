5129a211ccb468caf3c470f64c7007d9abbc8ac9
Jeffrey Cody <jcody@redhat.com>
2012-03-21 21:54:31 +0100

    Revert "qmp: add query-block-jobs command"
    RH-Author: Jeffrey Cody <jcody@redhat.com>
    Message-id: <69e367bec0f328cd8c80cdcad27457d4b84a9b9e.1332362400.git.jcody@redhat.com>
    Patchwork-id: 38854
    O-Subject: [RHEL6.3 qemu-kvm PATCH v8 04/54] Revert "qmp: add query-block-jobs command"
    Bugzilla: 582475
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Marcelo Tosatti <mtosatti@redhat.com>
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    
    From: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
    
    This reverts commit 298a7536dae5069c9e7ffae5c76c25ff5c6f5533.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    Signed-off-by: Jeff Cody <jcody@redhat.com>
    ---
     blockdev.c      |   49 -------------------------------------------------
     blockdev.h      |    2 --
     monitor.c       |    8 --------
     qemu-monitor.hx |   34 ----------------------------------
     4 files changed, 0 insertions(+), 93 deletions(-)
    
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- blockdev.h
+++ blockdev.h
@@ -72,8 +72,6 @@ int do_change_block(Monitor *mon, const char *device,
                     const char *filename, const char *fmt);
 int simple_drive_add(Monitor *mon, const QDict *qdict, QObject **ret_data);
 int do_drive_del(Monitor *mon, const QDict *qdict, QObject **ret_data);
-void monitor_print_block_jobs(Monitor *mon, const QObject *data);
-void do_info_block_jobs(Monitor *mon, QObject **ret_data);
 int do_block_stream(Monitor *mon, const QDict *params, QObject **ret_data);
 int do_block_job_cancel(Monitor *mon, const QDict *params,
                         MonitorCompletion cb, void *opaque);
