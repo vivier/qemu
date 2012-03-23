df38914e5fad2f4f3e17b6a79b99ff77c123a000
Federico Simoncelli <fsimonce@redhat.com>
2012-03-23 12:12:57 -0300

    add drive-mirror command and HMP equivalent
    RH-Author: Federico Simoncelli <fsimonce@redhat.com>
    Message-id: <1332504778-17403-13-git-send-email-fsimonce@redhat.com>
    Patchwork-id: 38955
    O-Subject: [RHEL6.3 qemu-kvm PATCH v6 12/13] add drive-mirror command and HMP equivalent
    Bugzilla: 802284
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Jeffrey Cody <jcody@redhat.com>
    
    From: Paolo Bonzini <pbonzini@redhat.com>
    
    Since QMP transactions are supposed to take (pseudo) QMP commands,
    add a standalone drive-mirror command.
    
    The corresponding HMP command does not provide atomic snapshot+mirror,
    but it can still be used together with image streaming.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>
    
    BZ: 802284
    
    Based on an patch posted upstream:
    http://lists.gnu.org/archive/html/qemu-devel/2012-03/msg00902.html
    
    Deviations from upstream:
     - drive-mirror commands renamed with a downstream prefix
    ---
     blockdev.c       |   15 +++++++++++++++
     hmp.c            |   28 ++++++++++++++++++++++++++++
     hmp.h            |    1 +
     qapi-schema.json |   27 +++++++++++++++++++++++++++
     qemu-monitor.hx  |   35 +++++++++++++++++++++++++++++++++++
     5 files changed, 106 insertions(+), 0 deletions(-)
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- hmp.c
+++ hmp.c
@@ -23,6 +23,34 @@ static void hmp_handle_error(Monitor *mon, Error **errp)
     }
 }
 
+void hmp_drive_mirror(Monitor *mon, const QDict *qdict)
+{
+    const char *device = qdict_get_str(qdict, "device");
+    const char *filename = qdict_get_try_str(qdict, "target");
+    const char *format = qdict_get_try_str(qdict, "format");
+    int reuse = qdict_get_try_bool(qdict, "reuse", 0);
+    int no_backing = qdict_get_try_bool(qdict, "no-backing", 0);
+    enum NewImageMode mode;
+    Error *errp = NULL;
+
+    if (!filename) {
+        error_set(&errp, QERR_MISSING_PARAMETER, "target");
+        hmp_handle_error(mon, &errp);
+        return;
+    }
+
+    if (reuse) {
+        mode = NEW_IMAGE_MODE_EXISTING;
+    } else if (no_backing) {
+        mode = NEW_IMAGE_MODE_NO_BACKING_FILE;
+    } else {
+        mode = NEW_IMAGE_MODE_ABSOLUTE_PATHS;
+    }
+
+    qmp___com_redhat_drive_mirror(device, filename, !!format, format, true, mode, &errp);
+    hmp_handle_error(mon, &errp);
+}
+
 void hmp_snapshot_blkdev(Monitor *mon, const QDict *qdict)
 {
     const char *device = qdict_get_str(qdict, "device");
