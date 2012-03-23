1c8147d897c184ecb247cace21ab5a995f7eec5e
Federico Simoncelli <fsimonce@redhat.com>
2012-03-23 12:12:47 -0300

    qapi: Convert blockdev_snapshot_sync
    RH-Author: Federico Simoncelli <fsimonce@redhat.com>
    Message-id: <1332504778-17403-3-git-send-email-fsimonce@redhat.com>
    Patchwork-id: 38951
    O-Subject: [RHEL6.3 qemu-kvm PATCH v6 02/13] qapi: Convert blockdev_snapshot_sync
    Bugzilla: 802284
    RH-Acked-by: Jeffrey Cody <jcody@redhat.com>
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    RH-Acked-by: Laszlo Ersek <lersek@redhat.com>
    
    From: Luiz Capitulino <lcapitulino@redhat.com>
    
    Unfortunately, this conversion required an additional change.
    
    In the old QMP command, the 'snapshot-file' argument is specified as
    optional. The idea is to take the snapshot internally if 'snapshot-file'
    is not passed. However, internal snapshots are not supported yet so
    the command returns a MissingParamater error if 'snapshot-file' is not
    passed. Which makes the argument actually required and will cause
    compatibility breakage if we change that in the future.
    
    To fix this the QAPI converted blockdev_snapshot_sync command makes the
    'snapshot-file' argument required. Again, in practice it's actually required,
    so this is not incompatible.
    
    If we do implement internal snapshots someday, we'll need a new argument
    for it.
    
    Note that this discussion doesn't affect HMP.
    
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    
    BZ: 802284
    
    (cherry picked from 6106e2492fe0080ad85d4862dec9c3bebc57b2f0)
    
    RHEL6 notes: hmp.c and hmp.h were added taking bits from 48a32be (qapi:
                 convert query-name) and 0cfd6a9 (qapi: Convert memsave).
                 Both patches had several dependencies and it wasn't
                 possible to backport them completely.
    
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>
    ---
     Makefile.objs    |    1 +
     blockdev.c       |   54 +++++++++++++++++++-----------------------------------
     hmp.c            |   44 ++++++++++++++++++++++++++++++++++++++++++++
     hmp.h            |   25 +++++++++++++++++++++++++
     monitor.c        |    1 +
     qapi-schema.json |   29 +++++++++++++++++++++++++++++
     qemu-monitor.hx  |    6 +++---
     7 files changed, 122 insertions(+), 38 deletions(-)
     create mode 100644 hmp.c
     create mode 100644 hmp.h
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

---
+++ hmp.c
@@ -0,0 +1,44 @@
+/*
+ * Human Monitor Interface
+ *
+ * Copyright IBM, Corp. 2011
+ *
+ * Authors:
+ *  Anthony Liguori   <aliguori@us.ibm.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ *
+ */
+
+#include "hmp.h"
+#include "qmp-commands.h"
+
+#ifdef CONFIG_LIVE_SNAPSHOTS
+static void hmp_handle_error(Monitor *mon, Error **errp)
+{
+    if (error_is_set(errp)) {
+        monitor_printf(mon, "%s\n", error_get_pretty(*errp));
+        error_free(*errp);
+    }
+}
+
+void hmp_snapshot_blkdev(Monitor *mon, const QDict *qdict)
+{
+    const char *device = qdict_get_str(qdict, "device");
+    const char *filename = qdict_get_try_str(qdict, "snapshot-file");
+    const char *format = qdict_get_try_str(qdict, "format");
+    Error *errp = NULL;
+
+    if (!filename) {
+        /* In the future, if 'snapshot-file' is not specified, the snapshot
+           will be taken internally. Today it's actually required. */
+        error_set(&errp, QERR_MISSING_PARAMETER, "snapshot-file");
+        hmp_handle_error(mon, &errp);
+        return;
+    }
+
+    qmp_blockdev_snapshot_sync(device, filename, !!format, format, &errp);
+    hmp_handle_error(mon, &errp);
+}
+#endif
