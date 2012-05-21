c25cc7490e82ed1cfae5b8bd4373da8c7474ae31
Eduardo Habkost <ehabkost@redhat.com>
2012-05-21 13:54:41 +0200

    Expose CPUID leaf 7 only for -cpu host (v2)
    RH-Author: Eduardo Habkost <ehabkost@redhat.com>
    Message-id: <1337608481-29002-3-git-send-email-ehabkost@redhat.com>
    Patchwork-id: 39750
    O-Subject: [RHEL6.3 qemu-kvm PATCH 2/2] Expose CPUID leaf 7 only for -cpu host (v2)
    Bugzilla: 819562
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Gleb Natapov <gleb@redhat.com>
    RH-Acked-by: Marcelo Tosatti <mtosatti@redhat.com>
    
    Bugzilla: 819562
    Upstream status:
        - v1 submitted at:
          Message-ID: <20120517162655.GQ16951@otherpad.lan.raisama.net>
          http://article.gmane.org/gmane.comp.emulators.kvm.devel/91040
        - v2 will be submitted in a few minutes
    
    Changes v1 -> v2:
     - Check for kvm_enabled() before setting cpuid_7_0_ebx_features
       (patch 1/2 is unchanged)
    
    Description of the bug:
    
    Since QEMU 0.15, the CPUID information on CPUID[EAX=7,ECX=0] is being
    returned unfiltered to the guest, directly from the GET_SUPPORTED_CPUID
    return value.
    
    The problem is that this makes the resulting CPU feature flags
    unpredictable and dependent on the host CPU and kernel version. This
    breaks live-migration badly if migrating from a host CPU that supports
    some features on that CPUID leaf (running a recent kernel) to a kernel
    or host CPU that doesn't support it.
    
    Migration also is incorrect (the virtual CPU changes under the guest's
    feet) if you migrate in the opposite direction (from an old CPU/kernel
    to a new CPU/kernel), but with less serious consequences (guests
    normally query CPUID information only once on boot).
    
    Fortunately, the bug affects only users using cpudefs with level >= 7.
    
    The right behavior should be to explicitly enable those features on
    [cpudef] config sections or on the "-cpu" command-line arguments. Right
    now there is no predefined CPU model on QEMU that has those features:
    the latest Intel model we have is Sandy Bridge.
    
    This patch changes QEMU to enable those features only if "-cpu host" is being
    used (as we don't have any pre-defined CPU model that actually have those
    features). Later we can make those features properly configurable on [cpudef]
    and -cpu configuration.
    
    One problem is: with this patch, users with the following setup:
    - Running QEMU 1.0;
    - Using a cpudef having level >= 7;
    - Running a kernel that supports the features on CPUID leaf 7; and
    - Running on a CPU that supports some features on CPUID leaf 7
    won't be able to live-migrate to QEMU 1.1. But for these users
    live-migration is already broken (they can't live-migrate to hosts with
    older CPUs or older kernels, already), I don't see how to avoid this
    problem.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    ---
     target-i386/cpu.h   |    2 ++
     target-i386/cpuid.c |   21 +++++++++++++++------
     2 files changed, 17 insertions(+), 6 deletions(-)
    
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- target-i386/cpu.h
+++ target-i386/cpu.h
@@ -701,6 +701,8 @@ typedef struct CPUX86State {
     uint32_t cpuid_model[12];
     uint32_t cpuid_ext2_features;
     uint32_t cpuid_ext3_features;
+    /* Flags from CPUID[EAX=7,ECX=0].EBX */
+    uint32_t cpuid_7_0_ebx;
     uint32_t cpuid_apic_id;
     int cpuid_vendor_override;
 
