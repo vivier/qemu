vivier/qemu

commit e1e5e2f3c6fad972872378594cedaf06bb50ac36

Add PIIX4 properties to control PM system states.
RH-Author: Gleb Natapov <gleb@redhat.com>
Message-id: <20120903083650.GC27250@redhat.com>
Patchwork-id: 41579
O-Subject: [PATCH RHEL6.4] Add PIIX4 properties to control PM system states.
Bugzilla: 827503
RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
RH-Acked-by: Igor Mammedov <imammedo@redhat.com>
RH-Acked-by: Laszlo Ersek <lersek@redhat.com>

This patch adds two things. First it allows QEMU to distinguish between
regular powerdown and S4 powerdown. Later separate QMP notification will
be added for S4 powerdown. Second it allows S3/S4 states to be disabled
from QEMU command line. Some guests known to be broken with regards to
power management, but allow to use it anyway. Using new properties
management will be able to disable S3/S4 for such guests.

Supported system state are passed to a firmware using new fw_cfg file.
The file contains  6 byte array. Each byte represents one system
state. If byte at offset X has its MSB set it means that system state
X is supported and to enter it guest should use the value from lowest 3
bits.

Signed-off-by: Gleb Natapov <gleb@redhat.com>
Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

Upstream: 459ae5ea5ad682c2b3220beb244d4102c1a4e332
BZ: 827503

Signed-off-by: Gleb Natapov <gleb@redhat.com>
--
			Gleb.

Signed-off-by: Michal Novotny <minovotn@redhat.com>

hw/acpi.c
@@ -84,6 +84,10 @@ typedef struct PIIX4PMState {
     struct pci_status pci0_status;
     uint32_t pci0_hotplug_enable;
     uint32_t pci0_slot_device_present;
+
+    uint8_t disable_s3;
+    uint8_t disable_s4;
+    uint8_t s4_val;
 } PIIX4PMState;
 
 #define RSM_STS (1 << 15)
@@ -219,6 +223,9 @@ static void pm_ioport_writew(void *opaque, uint32_t addr, uint32_t val)
                     qemu_system_suspend_request();
                     break;
                 default:
+                    if (sus_typ == s->s4_val) { /* S4 request */
+                        qemu_system_shutdown_request();
+                    }
                     break;
                 }
             }
@@ -726,7 +733,7 @@ static int piix4_pm_initfn(PCIDevice *dev)
 }
 
 i2c_bus *piix4_pm_init(PCIBus *bus, int devfn, uint32_t smb_io_base,
-                       qemu_irq sci_irq)
+                       qemu_irq sci_irq, void *fw_cfg)
 {
     PCIDevice *dev;
     PIIX4PMState *s;
@@ -738,6 +745,14 @@ i2c_bus *piix4_pm_init(PCIBus *bus, int devfn, uint32_t smb_io_base,
     s->irq = sci_irq;
 
     qdev_init_nofail(&dev->qdev);
+
+    if (fw_cfg) {
+        uint8_t suspend[6] = {128, 0, 0, 129, 128, 128};
+        suspend[3] = 1 | ((!s->disable_s3) << 7);
+        suspend[4] = s->s4_val | ((!s->disable_s4) << 7);
+
+        fw_cfg_add_file(fw_cfg, "etc/system-states", g_memdup(suspend, 6), 6);
+    }
 
     return s->smbus;
 }
@@ -752,6 +767,9 @@ static PCIDeviceInfo piix4_pm_info = {
     .config_write       = pm_write_config,
     .qdev.props         = (Property[]) {
         DEFINE_PROP_UINT32("smb_io_base", PIIX4PMState, smb_io_base, 0),
+        DEFINE_PROP_UINT8("disable_s3", PIIX4PMState, disable_s3, 0),
+        DEFINE_PROP_UINT8("disable_s4", PIIX4PMState, disable_s4, 0),
+        DEFINE_PROP_UINT8("s4_val", PIIX4PMState, s4_val, 2),
         DEFINE_PROP_END_OF_LIST(),
     }
 };

hw/ipf.c
@@ -601,7 +601,7 @@ static void ipf_init1(ram_addr_t ram_size,
         i2c_bus *smbus;
 
         /* TODO: Populate SPD eeprom data.  */
-        smbus = piix4_pm_init(pci_bus, piix3_devfn + 3, 0xb100, i8259[9]);
+        smbus = piix4_pm_init(pci_bus, piix3_devfn + 3, 0xb100, i8259[9], NULL);
         for (i = 0; i < 8; i++) {
             DeviceState *eeprom;
             eeprom = qdev_create((BusState *)smbus, "smbus-eeprom");

hw/mips_malta.c
@@ -927,7 +927,7 @@ void mips_malta_init (ram_addr_t ram_size,
     isa_bus_irqs(i8259);
     pci_piix4_ide_init(pci_bus, hd, piix4_devfn + 1);
     usb_uhci_piix4_init(pci_bus, piix4_devfn + 2);
-    smbus = piix4_pm_init(pci_bus, piix4_devfn + 3, 0x1100, isa_reserve_irq(9));
+    smbus = piix4_pm_init(pci_bus, piix4_devfn + 3, 0x1100, isa_reserve_irq(9), NULL);
     eeprom_buf = qemu_mallocz(8 * 256); /* XXX: make this persistent */
     for (i = 0; i < 8; i++) {
         /* TODO: Populate SPD eeprom data.  */

hw/pc.c
@@ -1343,7 +1343,7 @@ static void pc_init1(ram_addr_t ram_size,
 
         /* TODO: Populate SPD eeprom data.  */
         smbus = piix4_pm_init(pci_bus, piix3_devfn + 3, 0xb100,
-                              isa_reserve_irq(9));
+                              isa_reserve_irq(9), fw_cfg);
         for (i = 0; i < 8; i++) {
             DeviceState *eeprom;
             eeprom = qdev_create((BusState *)smbus, "smbus-eeprom");

hw/pc.h
@@ -120,7 +120,7 @@ int acpi_table_add(const char *table_desc);
 
 /* acpi_piix.c */
 i2c_bus *piix4_pm_init(PCIBus *bus, int devfn, uint32_t smb_io_base,
-                       qemu_irq sci_irq);
+                       qemu_irq sci_irq, void *fw_cfg);
 void piix4_smbus_register_device(SMBusDevice *dev, uint8_t addr);
 void piix4_acpi_system_hot_add_init(PCIBus *bus, const char *model);
 

commit b781952b82fd491a42f12149629a87072afb3407

Bugzilla: 856851 Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=4860099 Upstream: internal issue Test: tested in localhost
'make rh-rpms' doesn't work, error was introduced by commit e1e5e2f3

cc1: warnings being treated as errors
/qemu/redhat/rpm/BUILD/qemu-kvm-0.12.1.2/hw/acpi.c: In function
‘piix4_pm_init’:
/qemu/redhat/rpm/BUILD/qemu-kvm-0.12.1.2/hw/acpi.c:754: error:
implicit declaration of function ‘fw_cfg_add_file’

Signed-off-by: Amos Kong <akong@redhat.com>

hw/acpi.c
@@ -26,6 +26,7 @@
 #include "qemu-kvm.h"
 #include "string.h"
 #include "ioport.h"
+#include "fw_cfg.h"
 
 //#define DEBUG
 