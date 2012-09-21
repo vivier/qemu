dde0ee251346941101a486fa4dccf8fac1f53d6b
Gerd Hoffmann <kraxel@redhat.com>
2012-09-21 08:26:31 +0200

    spice: switch to queue for vga mode updates
    RH-Author: Gerd Hoffmann <kraxel@redhat.com>
    Message-id: <1348215994-27217-2-git-send-email-kraxel@redhat.com>
    Patchwork-id: 42155
    O-Subject: [RHEL-6.4 qemu-kvm PATCH 1/4] spice: switch to queue for vga mode updates
    Bugzilla: 854528
    RH-Acked-by: Hans de Goede <hdegoede@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Alon Levy <alevy@redhat.com>
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    
    Conflicts:
    
    	ui/spice-display.c
    
    upstream: b1af98ba3e4c4fd44f233fc7240df2612baeb1c2

--- hw/qxl.c
+++ hw/qxl.c
@@ -518,9 +518,9 @@ static int interface_get_command(QXLInstance *sin, struct QXLCommandExt *ext)
         dprint(qxl, 2, "%s: vga\n", __FUNCTION__);
         ret = false;
         qemu_mutex_lock(&qxl->ssd.lock);
-        if (qxl->ssd.update != NULL) {
-            update = qxl->ssd.update;
-            qxl->ssd.update = NULL;
+        update = QTAILQ_FIRST(&qxl->ssd.updates);
+        if (update != NULL) {
+            QTAILQ_REMOVE(&qxl->ssd.updates, update, next);
             *ext = update->ext;
             ret = true;
         }
