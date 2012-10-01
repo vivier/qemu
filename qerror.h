4ea7972b8d98b9f3f005aee233891608b8e952a1
Amos Kong <akong@redhat.com>
2012-10-01 14:12:43 +0200

    qerror: drop QERR_SOCKET_CONNECT_IN_PROGRESS
    RH-Author: Amos Kong <akong@redhat.com>
    Message-id: <1349100767-9066-11-git-send-email-akong@redhat.com>
    Patchwork-id: 42569
    O-Subject: [RHEL-6.4 qemu-kvm PATCH v7 10/14] qerror: drop QERR_SOCKET_CONNECT_IN_PROGRESS
    Bugzilla: 680356
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Markus Armbruster <armbru@redhat.com>
    RH-Acked-by: Orit Wasserman <owasserm@redhat.com>
    
    From: Luiz Capitulino <lcapitulino@redhat.com>
    
    Unused since last commit.
    
    (Cherry-picked from commit 75b81cf0f26f5bb7ada583fc434835adf03f8b77)
    
    Conflicts:
        qerror.h
    
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Amos Kong <akong@redhat.com>
    ---
     qerror.c |    4 ----
     qerror.h |    3 ---
     2 files changed, 0 insertions(+), 7 deletions(-)
    
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- qerror.h
+++ qerror.h
@@ -214,9 +214,6 @@ QError *qobject_to_qerror(const QObject *obj);
 #define QERR_QGA_COMMAND_FAILED \
     "{ 'class': 'QgaCommandFailed', 'data': { 'message': %s } }"
 
-#define QERR_SOCKET_CONNECT_IN_PROGRESS \
-    "{ 'class': 'SockConnectInprogress', 'data': {} }"
-
 #define QERR_SOCKET_CONNECT_FAILED \
     "{ 'class': 'SockConnectFailed', 'data': {} }"
 
