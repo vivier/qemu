d8939d9af7bcc9601cbb553ac540f6dd77e503b3
Amos Kong <akong@redhat.com>
2012-10-01 14:12:41 +0200

    net: inet_connect(), inet_connect_opts(): add in_progress argument
    RH-Author: Amos Kong <akong@redhat.com>
    Message-id: <1349100767-9066-9-git-send-email-akong@redhat.com>
    Patchwork-id: 42567
    O-Subject: [RHEL-6.4 qemu-kvm PATCH v7 08/14] net: inet_connect(), inet_connect_opts(): add in_progress argument
    Bugzilla: 680356
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Markus Armbruster <armbru@redhat.com>
    RH-Acked-by: Orit Wasserman <owasserm@redhat.com>
    
    From: Luiz Capitulino <lcapitulino@redhat.com>
    
    It's used to indicate the special case where a valid file-descriptor
    is returned (ie. success) but the connection can't be completed
    w/o blocking.
    
    This is needed because QERR_SOCKET_CONNECT_IN_PROGRESS is not
    treated like an error and a future commit will drop it.
    
    (Cherry-picked from commit 02a08fef079469c005d48fe2d181f0e0eb5752ae)
    
    Conflicts:
        nbd.c
        ui/vnc.c
    
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Amos Kong <akong@redhat.com>
    ---
     migration-tcp.c |    2 +-
     qemu-char.c     |    2 +-
     qemu-sockets.c  |   14 +++++++++++---
     qemu_socket.h   |    4 ++--
     vnc.c           |    2 +-
     5 files changed, 16 insertions(+), 8 deletions(-)
    
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- qemu_socket.h
+++ qemu_socket.h
@@ -43,8 +43,8 @@ int send_all(CharDriverState *chr, int fd, const void *buf, int len1);
 int inet_listen_opts(QemuOpts *opts, int port_offset, Error **errp);
 int inet_listen(const char *str, char *ostr, int olen,
                 int socktype, int port_offset, Error **errp);
-int inet_connect_opts(QemuOpts *opts, Error **errp);
-int inet_connect(const char *str, bool block, Error **errp);
+int inet_connect_opts(QemuOpts *opts, bool *in_progress, Error **errp);
+int inet_connect(const char *str, bool block, bool *in_progress, Error **errp);
 int inet_dgram_opts(QemuOpts *opts);
 const char *inet_strfamily(int family);
 
