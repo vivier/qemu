459211f90c3beee5aa5b189f90b4e97c680cdb3e
Michal Novotny <minovotn@redhat.com>
2013-05-07 18:35:55 +0200

    Revert "qemu-ga: ga_get_fd_handle(): abort if fd_counter overflows"
    This reverts commit eed13735cde3480817d45445b8786bb1a17c6c31.
    
    Reverting as asked by Laszlo in message <51892739.2030807@redhat.com>
    because of the ordering issue (most likely) related to supersed
    testing.
    
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- qemu-ga.c
+++ qemu-ga.c
@@ -889,13 +889,9 @@ int64_t ga_get_fd_handle(GAState *s, Error **errp)
     g_assert(!ga_is_frozen(s));
 
     handle = s->pstate.fd_counter++;
-
-    /* This should never happen on a reasonable timeframe, as guest-file-open
-     * would have to be issued 2^63 times */
-    if (s->pstate.fd_counter == INT64_MAX) {
-        abort();
+    if (s->pstate.fd_counter < 0) {
+        s->pstate.fd_counter = 0;
     }
-
     if (!write_persistent_state(&s->pstate, s->pstate_filepath)) {
         error_setg(errp, "failed to commit persistent state to disk");
     }
