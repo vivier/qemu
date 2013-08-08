vivier/qemu

commit 2a6976142274c43cdee2868c9a72a40cdf596761

use atexit for quit_timers
RH-Author: Amos Kong <akong@redhat.com>
Message-id: <1372169129-12142-1-git-send-email-akong@redhat.com>
Patchwork-id: 52215
O-Subject: [RHEL-6.5 qemu-kvm PATCH] use atexit for quit_timers
Bugzilla: 843797
RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
RH-Acked-by: Vlad Yasevich <vyasevic@redhat.com>
RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>

Bugzilla: 843797
Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=5945175
Test: tested in localhost

The last patch (http://patchwork.virt.bos.redhat.com/patch/45245/)
didn't fix the bug, but it's useful. This patch is an additional
fix.

Currently we quit timers when mainloop finishes. And net_cleanup()
will be called at normal process termination, it uses the released
alarm_timer, so qemu crash occurs.

Upstream also registers quit_timers() to be called at normal process
termination. This patch changes internal to quit timers at the end
of normal process termination. net_cleanup() can use effective
alarm_timer.

(manually backport commit 4260a7391190d2d01fac50ee3d738a15bab6b98e)

    commit 4260a7391190d2d01fac50ee3d738a15bab6b98e
    Author: Paolo Bonzini <pbonzini@redhat.com>
    Date:   Mon Sep 19 10:18:51 2011 +0200

        qemu-timer: use atexit for quit_timers

        Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

Signed-off-by: Amos Kong <akong@redhat.com>
---
 vl.c |    3 ++-
 1 files changed, 2 insertions(+), 1 deletions(-)

Signed-off-by: Michal Novotny <minovotn@redhat.com>

vl.c
@@ -6102,6 +6102,8 @@ int main(int argc, char **argv, char **envp)
     fips_set_state(true);
     loc_set_none();
 
+    atexit(&quit_timers);
+
     /* If no data_dir is specified then try to find it relative to the
        executable path.  */
     if (!data_dir) {
@@ -6623,7 +6625,6 @@ int main(int argc, char **argv, char **envp)
 #endif
 
     main_loop();
-    quit_timers();
 
     return 0;
 }

commit 312188db7a97b59995ab5ec7689d7c0a2937a0df

register exit function after starting timers
RH-Author: Amos Kong <akong@redhat.com>
Message-id: <1375866434-28876-1-git-send-email-akong@redhat.com>
Patchwork-id: 53032
O-Subject: [RHEL-6.5 qemu-kvm PATCH] register exit function after starting timers
Bugzilla: 843797
RH-Acked-by: Laszlo Ersek <lersek@redhat.com>
RH-Acked-by: Vlad Yasevich <vyasevic@redhat.com>
RH-Acked-by: Luiz Capitulino <lcapitulino@redhat.com>

Bugzilla: 843797
Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=6136472
Upstream: this bug was introduced by backport, upstream is ok
Test: tested in localhost

This patch fixes a regression bug that was introduced by commit
2a697614. quit_timer is registered before init_timer_alarm(),
quitting an uninited timer will cause a crash.

Moving quit_timers() to the above of init_timer_alarm(), it's
used in init_timer_alarm().

This patch calls atexit() inside of init_timer_alarm() as upstream,
That still happens before network initialization so the bug 843797
will be fixed.

Signed-off-by: Amos Kong <akong@redhat.com>
---
 vl.c |   15 +++++++--------
 1 files changed, 7 insertions(+), 8 deletions(-)

Signed-off-by: Michal Novotny <minovotn@redhat.com>

vl.c
@@ -1826,6 +1826,12 @@ static void win32_rearm_timer(struct qemu_alarm_timer *t)
 
 #endif /* _WIN32 */
 
+static void quit_timers(void)
+{
+    alarm_timer->stop(alarm_timer);
+    alarm_timer = NULL;
+}
+
 static int init_timer_alarm(void)
 {
     struct qemu_alarm_timer *t = NULL;
@@ -1845,17 +1851,12 @@ static int init_timer_alarm(void)
     }
 
     alarm_timer = t;
+    atexit(&quit_timers);
 
     return 0;
 
 fail:
     return err;
-}
-
-static void quit_timers(void)
-{
-    alarm_timer->stop(alarm_timer);
-    alarm_timer = NULL;
 }
 
 /***********************************************************/
@@ -6103,8 +6104,6 @@ int main(int argc, char **argv, char **envp)
     }
     fips_set_state(true);
     loc_set_none();
-
-    atexit(&quit_timers);
 
     /* If no data_dir is specified then try to find it relative to the
        executable path.  */