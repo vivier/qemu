b9fef831153b563e57aec022df7845a5024676a9
Eduardo Habkost <ehabkost@redhat.com>
2013-08-20 15:09:48 +0200

    pc: rhel6.x has x2apic present on Conroe/Penryn/Nehalem CPU models
    RH-Author: Eduardo Habkost <ehabkost@redhat.com>
    Message-id: <1377011392-9336-4-git-send-email-ehabkost@redhat.com>
    Patchwork-id: 53613
    O-Subject: [RHEL7 PATCH 3/7] pc: rhel6.x has x2apic present on Conroe/Penryn/Nehalem CPU models
    Bugzilla: 918907
    RH-Acked-by: Markus Armbruster <armbru@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    Bugzilla: 918907
    Upstream status: not applicable
    
    The Conroe/Penryn/Nehalem CPU models all have x2apic enabled on rhel6.x
    (added by RHEL-6 commit 19ab65a663cfdfa8365f3ec324af6b0ab26be5a1). Add
    compat bits to make sure they are set on the rhel6.x machine-types.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    ---
     hw/i386/pc_piix.c | 4 ++++
     1 file changed, 4 insertions(+)
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- hw/i386/pc_piix.c
+++ hw/i386/pc_piix.c
@@ -947,6 +947,10 @@ static QEMUMachine pc_machine_rhel700 = {
 
 static void pc_init_rhel650(QEMUMachineInitArgs *args)
 {
+    x86_cpu_compat_set_features("Conroe", FEAT_1_ECX, CPUID_EXT_X2APIC, 0);
+    x86_cpu_compat_set_features("Penryn", FEAT_1_ECX, CPUID_EXT_X2APIC, 0);
+    x86_cpu_compat_set_features("Nehalem", FEAT_1_ECX, CPUID_EXT_X2APIC, 0);
+    x86_cpu_compat_set_features("Westmere", FEAT_1_ECX, CPUID_EXT_X2APIC, 0);
     pc_init_rhel700(args);
 }
 
