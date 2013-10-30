7ef8a6814f6adc37a464032bc77e21595f13e7af
Michal Novotny <minovotn@redhat.com>
2013-10-30 14:28:09 +0100

    redhat: Various fixes in merge-from-pw.sh
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- redhat/merge-from-pw.sh
+++ redhat/merge-from-pw.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Activate Kerberos 5 keytab
+./home/patchwork/activate-keytab.sh > /dev/null
+
 MASTER="6.5"
 REPOBASE="/home/patchwork/git/qemu-kvm-rhel"
 BRANCHBASE="rhel"
@@ -33,11 +36,10 @@ fi
 
 r=$(echo $1 | tr -d ".0")
 
+branchname="$BRANCHBASE$r"
 if [ "$1" == "$MASTER" ]; then
 	branchname="$BRANCHBASE$rver"
 fi
-
-branchname="$BRANCHBASE$r"
 
 # Server path
 cd $rpath
@@ -78,11 +80,17 @@ export BUILD_TEST_SCRIPTED=1
 if [ "$failed" -eq 0 -a "$applied" -gt 0 ]; then
 	tmpFileR="$(mktemp)"
 
-	echo "This is automatic test results for automergetest build." > $tmpFileR
+	make -C redhat rh-brew-at-covscan LOCALVERSION="automergetest" | egrep 'Task ID seems to be|\(id |New Coverity test job has been created:' | awk '{ if (match($0, "seems") != 0) { split($0, a, "be "); gsub(/\./, "", a[2]); print "Brew ID: "a[2] }; if (match($0, "id") != 0) { split($0, a, "id "); gsub(/)\047/, "", a[2]); print "Autotest job ID: "a[2] }; if (match($0, "Coverity test job has been created:") != 0) { split($0, a, ": "); split(a[2], b, " ."); print "Coverity job ID: "b[1] } }' 2> /dev/null > test.tmp
+
+	echo "Hi," > $tmpFileR
+	echo "these are test results for automatic mergetest build." >> $tmpFileR
 	echo "Relevant identifiers are:" >> $tmpFileR
-	make -C redhat rh-brew-at-covscan LOCALVERSION="automergetest" | egrep 'Task ID seems to be|\(id |New Coverity test job has been created:' | awk '{ if (match($0, "seems") != 0) { split($0, a, "be "); gsub(/\./, "", a[2]); print "Brew ID: "a[2] }; if (match($0, "id") != 0) { split($0, a, "id "); gsub(/)\047/, "", a[2]); print "Autotest job ID: "a[2] }; if (match($0, "Coverity test job has been created:") != 0) { split($0, a, ": "); split(a[2], b, " ."); print "Coverity job ID: "b[1] } }' >> $tmpFileR
+	echo >> $tmpFileR
+	cat test.tmp >> $tmpFileR
 	echo >> $tmpFileR
 	cat /home/patchwork/public_reports/qemu-kvm-RHEL-6.5/short_report.txt  | grep PATCH | awk '{ split($0, a, " "); if (a[1] != "123456") { if (match(a[5], "N") == 0) print $0 } }' >> $tmpFileR
+	echo >> $tmpFileR
+	echo "All patches on queue ($applied) were applied successfully" >> $tmpFileR
 
 	for addr in ${REPORT_EMAIL[@]}
 	do
