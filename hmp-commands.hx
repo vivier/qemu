3180ddce875c175e9de1c9371a7512c7458cfaca
Miroslav Rezanina <mrezanin@redhat.com>
2013-11-06 12:36:04 +0100

    monitor: Remove host_net_add/remove for Red Hat Enterprise Linux
    RH-Author: Miroslav Rezanina <mrezanin@redhat.com>
    Message-id: <b28f4458b4470c9a6fcbf5085c3a1bb4d7a0a5eb.1383741033.git.mrezanin@redhat.com>
    Patchwork-id: 55519
    O-Subject: [RHEL7 qemu-kvm PATCH v2 4/4] monitor: Remove host_net_add/remove for Red Hat Enterprise Linux
    Bugzilla: 1010858
    RH-Acked-by: Michal Novotny <minovotn@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Orit Wasserman <owasserm@redhat.com>
    RH-Acked-by: Eric Blake <eblake@redhat.com>
    
    From: Miroslav Rezanina <mrezanin@redhat.com>
    
    This is forwardport of RHEL-6 commit dd94505bd1b826db0c7e155ccee5c24f77987f16:
    
        monitor: Remove host_net_add/remove for Red Hat Enterprise Linux
    
        The host_net_add/remove commands are replaced by netdev_add/del. Remove
        them.
    
        Signed-off-by: Amit Shah <amit.shah@redhat.com>
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>
    ---
     hmp-commands.hx | 2 ++
     1 file changed, 2 insertions(+)
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- hmp-commands.hx
+++ hmp-commands.hx
@@ -1215,6 +1215,7 @@ STEXI
 Inject PCIe AER error
 ETEXI
 
+#if 0 /* Disabled for Red Hat Enterprise Linux */
     {
         .name       = "host_net_add",
         .args_type  = "device:s,opts:s?",
@@ -1244,6 +1245,7 @@ STEXI
 @findex host_net_remove
 Remove host VLAN client.
 ETEXI
+#endif
 
     {
         .name       = "netdev_add",
