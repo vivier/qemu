c7cc19af9467dfdace27d35acdea3f87fe228252
Laszlo Ersek <lersek@redhat.com>
2014-03-22 03:31:08 +0100

    dump: add API to write dump pages
    RH-Author: Laszlo Ersek <lersek@redhat.com>
    Message-id: <1395459071-19118-17-git-send-email-lersek@redhat.com>
    Patchwork-id: 58227
    O-Subject: [RHEL-6.6 qemu-kvm PATCH 16/19] dump: add API to write dump pages
    Bugzilla: 1035162
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Dr. David Alan Gilbert (git) <dgilbert@redhat.com>
    RH-Acked-by: Luiz Capitulino <lcapitulino@redhat.com>
    
    From: qiaonuohan <qiaonuohan@cn.fujitsu.com>
    
    functions are used to write page to vmcore. vmcore is written page by page.
    page desc is used to store the information of a page, including a page's size,
    offset, compression format, etc.
    
    Signed-off-by: Qiao Nuohan <qiaonuohan@cn.fujitsu.com>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    (cherry picked from commit d12f57ec6640d36e380367a0ab6ab9f3f29b6d51)
    
    Conflicts:
    	dump.c
    	dump.h
    
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    ---
     dump.h |   7 ++
     dump.c | 231 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     2 files changed, 238 insertions(+)
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- dump.h
+++ dump.h
@@ -153,4 +153,11 @@ typedef struct DataCache {
     off_t offset;       /* offset of the file */
 } DataCache;
 
+typedef struct QEMU_PACKED PageDescriptor {
+    uint64_t offset;                /* the offset of the page data*/
+    uint32_t size;                  /* the size of this dump page */
+    uint32_t flags;                 /* flags */
+    uint64_t page_flags;            /* page flags */
+} PageDescriptor;
+
 #endif
