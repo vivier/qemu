8887dd1f4b5a76f131a4522f7ad73101413a4927
Kevin Wolf <kwolf@redhat.com>
2014-03-25 11:45:34 +0100

    qcow2: Limit snapshot table size
    RH-Author: Kevin Wolf <kwolf@redhat.com>
    Message-id: <1395744364-16049-16-git-send-email-kwolf@redhat.com>
    Patchwork-id: n/a
    O-Subject: [EMBARGOED RHEL-6.6/6.5.z qemu-kvm PATCH v2 15/45]
               qcow2: Limit snapshot table size
    Bugzilla: 1079518
    RH-Acked-by: Max Reitz <mreitz@redhat.com>
    RH-Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    RH-Acked-by: Jeff Cody <jcody@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1079518
    Upstream status: Series embargoed
    
    Even with a limit of 64k snapshots, each snapshot could have a filename
    and an ID with up to 64k, which would still lead to pretty large
    allocations, which could potentially lead to qemu aborting. Limit the
    total size of the snapshot table to an average of 1k per entry when
    the limit of 64k snapshots is fully used. This should be plenty for any
    reasonable user.
    
    This also fixes potential integer overflows of s->snapshot_size.
    
    Suggested-by: Max Reitz <mreitz@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    
    Conflicts:
    	block/qcow2.h
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/qcow2.h
+++ block/qcow2.h
@@ -46,6 +46,10 @@
  * (128 GB for 512 byte clusters, 2 EB for 2 MB clusters) */
 #define QCOW_MAX_REFTABLE_SIZE 0x800000
 
+/* Allow for an average of 1k per snapshot table entry, should be plenty of
+ * space for snapshot names and IDs */
+#define QCOW_MAX_SNAPSHOTS_SIZE (1024 * QCOW_MAX_SNAPSHOTS)
+
 /* indicate that the refcount of the referenced cluster is exactly one. */
 #define QCOW_OFLAG_COPIED     (1LL << 63)
 /* indicate that the cluster is compressed (they never have the copied flag) */
