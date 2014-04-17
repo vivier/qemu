2a9c9c31f756c4d240f14717f376dda187a9b630
Laszlo Ersek <lersek@redhat.com>
2014-04-17 11:13:59 +0200

    qemu_loadvm_state(): shadow SeaBIOS for VM incoming from RHEL-6 host
    RH-Author: Laszlo Ersek <lersek@redhat.com>
    Message-id: <1397733239-8835-1-git-send-email-lersek@redhat.com>
    Patchwork-id: 58498
    O-Subject: [RHEL-7.0 0day qemu-kvm PATCH v2] qemu_loadvm_state(): shadow SeaBIOS for VM incoming from RHEL-6 host
    Bugzilla: 1103579
    RH-Acked-by: Dr. David Alan Gilbert (git) <dgilbert@redhat.com>
    RH-Acked-by: Marcel Apfelbaum <marcel.a@redhat.com>
    RH-Acked-by: Eduardo Habkost <ehabkost@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1027565
    Brew:     https://brewweb.devel.redhat.com/taskinfo?taskID=7352678
    
    RHEL-only patch.
    
    SeaBIOS's shadowing logic has no effect on "pc.ram" (only on "pc.bios" and
    "pc.rom") when it runs on the RHEL-6 emulator. When such a guest is
    migrated to the RHEL-7 emulator, where the PAM registers actually work,
    these two UMBs under 1MB simply disappear from the guest's view, breaking
    reboot and S3 resume.
    
    Tested extensively by QE.
    
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    ---
    
    Notes:
        Changes in v2:
        - print message to stderr when we shadow the UMBs manually [Dave]
        - also copy C and D segments from pc.rom to pc.ram
        - move to qemu_loadvm_state() from ram_load() -- we must shadow only
          when all RAMBlocks have been loaded [Dave]
    
     include/sysemu/sysemu.h |  1 +
     hw/i386/pc_piix.c       |  1 +
     savevm.c                | 66 +++++++++++++++++++++++++++++++++++++++++++++++++
     3 files changed, 68 insertions(+)
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- hw/i386/pc_piix.c
+++ hw/i386/pc_piix.c
@@ -1073,6 +1073,7 @@ static void pc_compat_rhel650(QEMUMachineInitArgs *args)
     rom_file_has_mr = false;
     has_acpi_build = false;
     gigabyte_align = false;
+    shadow_bios_after_incoming = true;
 }
 
 static void pc_init_rhel650(QEMUMachineInitArgs *args)
