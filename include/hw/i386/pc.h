79e2796df3609593ba745f9ea4228ec58535a993
Markus Armbruster <armbru@redhat.com>
2014-08-11 07:54:43 +0200

    rhel: apic.version=0x11 for machine types <= 7.0
    Message-id: <1407743689-13553-10-git-send-email-armbru@redhat.com>
    Patchwork-id: 60513
    O-Subject: [PATCH RHEV-7.1 qemu-kvm-rhev 09/15] rhel: apic.version=0x11 for machine types <= 7.0
    Bugzilla: 1118665
    RH-Acked-by: Eduardo Habkost <ehabkost@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    RH-Acked-by: Dr. David Alan Gilbert (git) <dgilbert@redhat.com>
    
    Update RHEL compatibility properties for upstream commit aa93200.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    ---
     include/hw/i386/pc.h | 4 ++++
     1 file changed, 4 insertions(+)
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -533,6 +533,10 @@ bool e820_get_entry(int, uint32_t, uint64_t *, uint64_t *);
             .property = "memory-hotplug-support",\
             .value    = "off",\
         },{\
+            .driver   = "apic",\
+            .property = "version",\
+            .value    = stringify(0x11),\
+        },{\
             .driver   = "PIIX4_PM",\
             .property = "acpi-pci-hotplug-with-bridge-support",\
             .value    = "off",\
