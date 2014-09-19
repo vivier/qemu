9482cf37df20d18140b24751972697f9349f1441
Jeffrey Cody <jcody@redhat.com>
2014-09-19 03:18:58 +0200

    block: add backing-file option to block-stream
    Message-id: <5b8611fab581db6b4c5eb31c998378090819268d.1411096194.git.jcody@redhat.com>
    Patchwork-id: 61312
    O-Subject: [PATCH qemu-kvm-rhev RHEL7.0.z 3/4] block: add backing-file option to block-stream
    Bugzilla: 1122925
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    RH-Acked-by: Eric Blake <eblake@redhat.com>
    RH-Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    
    On some image chains, QEMU may not always be able to resolve the
    filenames properly, when updating the backing file of an image
    after a block job.
    
    For instance, certain relative pathnames may fail, or drives may
    have been specified originally by file descriptor (e.g. /dev/fd/???),
    or a relative protocol pathname may have been used.
    
    In these instances, QEMU may lack the information to be able to make
    the correct choice, but the user or management layer most likely does
    have that knowledge.
    
    With this extension to the block-stream api, the user is able to change
    the backing file of the active layer as part of the block-stream
    operation.
    
    This allows the change to be 'safe', in the sense that if the attempt
    to write the active image metadata fails, then the block-stream
    operation returns failure, without disrupting the guest.
    
    If a backing file string is not specified in the command, the backing
    file string to use is determined in the same manner as it was
    previously.
    
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Jeff Cody <jcody@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    (cherry picked from commit 13d8cc515dfcf5574077f964332d34890c0101d0)
    
    Conflicts:
    	block/stream.c
    	blockdev.c
    	qapi/block-core.json
    
    RHEL7 Notes:  Conflicts due to: surrounding context differences, and
                                    and the qapi json not being split into
                                    separate files like upstream.  Also
                                    upstream used 1024 for local backing
                                    file string size, and downstream we
                                    already use PATH_MAX instead.
    
    Signed-off-by: Jeff Cody <jcody@redhat.com>
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- hmp.c
+++ hmp.c
@@ -1046,7 +1046,7 @@ void hmp_block_stream(Monitor *mon, const QDict *qdict)
     const char *base = qdict_get_try_str(qdict, "base");
     int64_t speed = qdict_get_try_int(qdict, "speed", 0);
 
-    qmp_block_stream(device, base != NULL, base,
+    qmp_block_stream(device, base != NULL, base, false, NULL,
                      qdict_haskey(qdict, "speed"), speed,
                      BLOCKDEV_ON_ERROR_REPORT, true, &error);
 
