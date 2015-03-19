c1339067c6364052ad6e07fcd808e8bd0df2235d
Max Reitz <mreitz@redhat.com>
2015-03-18 19:22:20 -0500

    qcow2: Fix header extension size check
    RH-Author: Max Reitz <mreitz@redhat.com>
    Message-id: <1426706542-30384-38-git-send-email-mreitz@redhat.com>
    Patchwork-id: 64501
    O-Subject: [RHEL-6.7 qemu-kvm PATCH v2 37/39] qcow2: Fix header extension size check
    Bugzilla: 1129892
    RH-Acked-by: Jeffrey Cody <jcody@redhat.com>
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    RH-Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    
    From: Kevin Wolf <kwolf@redhat.com>
    
    BZ: 1129892
    
    After reading the extension header, offset is incremented, but not
    checked against end_offset any more. This way an integer overflow could
    happen when checking whether the extension end is within the allowed
    range, effectively disabling the check.
    
    This patch adds the missing check and a test case for it.
    
    Cc: qemu-stable@nongnu.org
    Reported-by: Max Reitz <mreitz@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Message-id: 1416935562-7760-2-git-send-email-kwolf@redhat.com
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit 2ebafc854d109ff09b66fb4dd62c2c53fc29754a)
    Signed-off-by: Jeff E. Nelson <jen@redhat.com>
    
    Conflicts:
    	block/qcow2.c
    	tests/qemu-iotests/080.out
    
    error_setg() vs. error_report().
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    ---
     block/qcow2.c              | 2 +-
     tests/qemu-iotests/080     | 2 ++
     tests/qemu-iotests/080.out | 3 +++
     3 files changed, 6 insertions(+), 1 deletion(-)

--- block/qcow2.c
+++ block/qcow2.c
@@ -115,7 +115,7 @@ static int qcow2_read_extensions(BlockDriverState *bs, uint64_t start_offset,
 #ifdef DEBUG_EXT
         printf("ext.magic = 0x%x\n", ext.magic);
 #endif
-        if (ext.len > end_offset - offset) {
+        if (offset > end_offset || ext.len > end_offset - offset) {
             error_report("Header extension too large");
             return -EINVAL;
         }
