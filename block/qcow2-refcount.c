52543f50ab9ca96d5b8b4197b1a6f5406432cb4b
Max Reitz <mreitz@redhat.com>
2015-03-18 19:21:58 -0500

    qcow2: Use qcow2_signal_corruption() for overlaps
    RH-Author: Max Reitz <mreitz@redhat.com>
    Message-id: <1426706542-30384-16-git-send-email-mreitz@redhat.com>
    Patchwork-id: 64484
    O-Subject: [RHEL-6.7 qemu-kvm PATCH v2 15/39] qcow2: Use qcow2_signal_corruption() for overlaps
    Bugzilla: 1129892
    RH-Acked-by: Jeffrey Cody <jcody@redhat.com>
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    RH-Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    
    BZ: 1129892
    
    Use the new function in case of a failed overlap check.
    
    This changes output in case of corruption, so adapt iotest 060's
    reference output accordingly.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Benoît Canet <benoit.canet@nodalink.com>
    Message-id: 1409926039-29044-4-git-send-email-mreitz@redhat.com
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    (cherry picked from commit adb435522b86b3fca2324cb8c94e17b55ae071f1)
    Signed-off-by: Jeff E. Nelson <jen@redhat.com>
    
    Conflicts:
    	block/qcow2-refcount.c
    	tests/qemu-iotests/060.out
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    ---
     block/qcow2-refcount.c     | 21 +++------------------
     tests/qemu-iotests/060.out |  6 +++---
     2 files changed, 6 insertions(+), 21 deletions(-)

--- block/qcow2-refcount.c
+++ block/qcow2-refcount.c
@@ -26,8 +26,6 @@
 #include "block_int.h"
 #include "block/qcow2.h"
 #include "range.h"
-#include "qemu-objects.h"
-#include "monitor.h"
 
 static int64_t alloc_clusters_noref(BlockDriverState *bs, uint64_t size);
 static int QEMU_WARN_UNUSED_RESULT update_refcount(BlockDriverState *bs,
@@ -1637,24 +1635,11 @@ int qcow2_pre_write_overlap_check(BlockDriverState *bs, int ign, int64_t offset,
         return ret;
     } else if (ret > 0) {
         int metadata_ol_bitnr = ffs(ret) - 1;
-        char *message;
-        QObject *data;
-
         assert(metadata_ol_bitnr < QCOW2_OL_MAX_BITNR);
 
-        fprintf(stderr, "qcow2: Preventing invalid write on metadata (overlaps "
-                "with %s); please use qemu-img check -r.\n",
-                metadata_ol_names[metadata_ol_bitnr]);
-        message = g_strdup_printf("Prevented %s overwrite",
-                metadata_ol_names[metadata_ol_bitnr]);
-        data = qobject_from_jsonf("{ 'device': %s, 'msg': %s, 'offset': %"
-                PRId64 ", 'size': %" PRId64 ", 'fatal': true }",
-                bs->device_name, message, offset, size);
-        monitor_protocol_event(QEVENT_BLOCK_IMAGE_CORRUPTED, data);
-        g_free(message);
-        qobject_decref(data);
-
-        bs->drv = NULL; /* make BDS unusable */
+        qcow2_signal_corruption(bs, true, offset, size, "Preventing invalid "
+                                "write on metadata (overlaps with %s)",
+                                metadata_ol_names[metadata_ol_bitnr]);
         return -EIO;
     }
 
