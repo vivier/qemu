b5b9a0497ed1bca434af8b321c2d5a87f4025489
Laurent Vivier <lvivier@redhat.com>
2015-06-25 13:44:35 +0200

    spapr_events: event-scan RTAS interface
    Message-id: <1435239881-28541-9-git-send-email-lvivier@redhat.com>
    Patchwork-id: 66490
    O-Subject: [RHEL7.2 qemu-kvm-rhev PATCH 08/14] spapr_events: event-scan RTAS interface
    Bugzilla: 1172478
    RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    
    From: Tyrel Datwyler <tyreld@linux.vnet.ibm.com>
    
    We don't actually rely on this interface to surface hotplug events, and
    instead rely on the similar-but-interrupt-driven check-exception RTAS
    interface used for EPOW events. However, the existence of this interface
    is needed to ensure guest kernels initialize the event-reporting
    interfaces which will in turn be used by userspace tools to handle these
    events, so we implement this interface here.
    
    Since events surfaced by this call are mutually exclusive to those
    surfaced via check-exception, we also update the RTAS event queue code
    to accept a boolean to mark/filter for events accordingly.
    
    Events of this sort are not currently generated by QEMU, but the interface
    has been tested by surfacing hotplug events via event-scan in place
    of check-exception.
    
    Signed-off-by: Tyrel Datwyler <tyreld@linux.vnet.ibm.com>
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Alexander Graf <agraf@suse.de>
    (cherry picked from commit 79853e18d904b0a4bcef62701d48559688007c93)
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- include/hw/ppc/spapr.h
+++ include/hw/ppc/spapr.h
@@ -510,6 +510,8 @@ int spapr_rtas_device_tree_setup(void *fdt, hwaddr rtas_addr,
 
 #define RTAS_ERROR_LOG_MAX      2048
 
+#define RTAS_EVENT_SCAN_RATE    1
+
 typedef struct sPAPRTCETable sPAPRTCETable;
 
 #define TYPE_SPAPR_TCE_TABLE "spapr-tce-table"
@@ -533,6 +535,7 @@ struct sPAPRTCETable {
 
 struct sPAPREventLogEntry {
     int log_type;
+    bool exception;
     void *data;
     QTAILQ_ENTRY(sPAPREventLogEntry) next;
 };
