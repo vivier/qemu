2fbb6570e075a99b3b25b4f557bae51b2cc0ee7b
Juan Quintela <quintela@redhat.com>
2015-07-28 15:47:04 +0200

    migration: create migration event
    Message-id: <1438098431-30847-20-git-send-email-quintela@redhat.com>
    Patchwork-id: 67177
    O-Subject: [RHEL-7 qemu-kvm PATCH 19/26] migration: create migration event
    Bugzilla: 580006
    RH-Acked-by: Alex Williamson <alex.williamson@redhat.com>
    RH-Acked-by: Amit Shah <amit.shah@redhat.com>
    RH-Acked-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    
    We have one argument that tells us what event has happened.
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    (cherry picked from commit 598cd2bda0845096d2f06500e45b4d0d399b384a)
    Signed-off-by: Juan Quintela <quintela@trasno.org>
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- migration/migration.c
+++ migration/migration.c
@@ -26,6 +26,7 @@
 #include "qmp-commands.h"
 #include "trace.h"
 #include "qapi/util.h"
+#include "qapi-event.h"
 
 #define MAX_THROTTLE  (32 << 20)      /* Migration speed throttling */
 
@@ -406,6 +407,7 @@ void qmp_migrate_set_capabilities(MigrationCapabilityStatusList *params,
 static void migrate_set_state(MigrationState *s, int old_state, int new_state)
 {
     if (atomic_cmpxchg(&s->state, old_state, new_state) == old_state) {
+        qapi_event_send_migration(new_state, &error_abort);
         trace_migrate_set_state(new_state);
     }
 }
