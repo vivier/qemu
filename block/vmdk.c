aea042fc3e0a8454edb4e9884635af63128b15d3
Fam Zheng <famz@redhat.com>
2016-02-15 09:28:19 +0100

    vmdk: Remove unnecessary initialization
    RH-Author: Fam Zheng <famz@redhat.com>
    Message-id: <1455528511-9357-7-git-send-email-famz@redhat.com>
    Patchwork-id: 69172
    O-Subject: [RHEL-7.3 qemu-kvm PATCH 06/18] vmdk: Remove unnecessary initialization
    Bugzilla: 1299250
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    RH-Acked-by: Max Reitz <mreitz@redhat.com>
    RH-Acked-by: Markus Armbruster <armbru@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1299250
    
    It will be assigned to the return value of vmdk_read_desc.
    
    Suggested-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Don Koch <dkoch@verizon.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Message-id: 1417649314-13704-6-git-send-email-famz@redhat.com
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit 9aeecbbc62ce52a94b2621a0d53567b5d4ed915d)
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- block/vmdk.c
+++ block/vmdk.c
@@ -916,7 +916,7 @@ exit:
 static int vmdk_open(BlockDriverState *bs, QDict *options, int flags,
                      Error **errp)
 {
-    char *buf = NULL;
+    char *buf;
     int ret;
     BDRVVmdkState *s = bs->opaque;
     uint32_t magic;
