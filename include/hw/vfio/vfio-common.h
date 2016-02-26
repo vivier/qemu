3ea920841ec01d5c2070ac871f238df83d9c6355
Alexey Kardashevskiy <aik@ozlabs.ru>
2016-02-26 17:00:20 +1100

    memory: Fix IOMMU replay base address
    Since a788f227 "memory: Allow replay of IOMMU mapping notifications"
    when new VFIO listener is added, all existing IOMMU mappings are
    replayed. However there is a problem that the base address of
    an IOMMU memory region (IOMMU MR) is ignored which is not a problem
    for the existing user (which is pseries) with its default 32bit DMA
    window starting at 0 but it is if there is another DMA window.
    
    This stores the IOMMU's offset_within_address_space and adjusts
    the IOVA before calling vfio_dma_map/vfio_dma_unmap.
    
    As the IOMMU notifier expects IOVA offset rather than the absolute
    address, this also adjusts IOVA in sPAPR H_PUT_TCE handler before
    calling notifier(s).
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

--- include/hw/vfio/vfio-common.h
+++ include/hw/vfio/vfio-common.h
@@ -90,6 +90,7 @@ typedef struct VFIOContainer {
 typedef struct VFIOGuestIOMMU {
     VFIOContainer *container;
     MemoryRegion *iommu;
+    hwaddr offset_within_address_space;
     Notifier n;
     QLIST_ENTRY(VFIOGuestIOMMU) giommu_next;
 } VFIOGuestIOMMU;
