cc64886012e7e919ef837eaa3019c97825506417
Paolo Bonzini <pbonzini@redhat.com>
2016-05-31 14:45:41 +0200

    dma-helpers: change BlockBackend to opaque value in DMAIOFunc
    RH-Author: Paolo Bonzini <pbonzini@redhat.com>
    Message-id: <1464705946-20914-3-git-send-email-pbonzini@redhat.com>
    Patchwork-id: 70505
    O-Subject: [RHEL7.3 qemu-kvm-rhev PATCH 2/7] dma-helpers: change BlockBackend to opaque value in DMAIOFunc
    Bugzilla: 1338043
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    RH-Acked-by: Fam Zheng <famz@redhat.com>
    RH-Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    
    Callers of dma_blk_io have no way to pass extra data to the DMAIOFunc,
    because the original callback and opaque are gone by the time DMAIOFunc
    is called.  On the other hand, the BlockBackend is usually derived
    from those extra data that you could pass to the DMAIOFunc (in the
    next patch, that would be the SCSIRequest).
    
    So change DMAIOFunc's prototype, decoupling it from blk_aio_readv
    and blk_aio_writev's.  The new prototype loses the BlockBackend
    and gains an extra opaque value which, in the case of dma_blk_readv
    and dma_blk_writev, is of course used for the BlockBackend.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit 8a8e63ebdd5a6605af6a9df07c62b2214e1a650c)
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- hw/ide/core.c
+++ hw/ide/core.c
@@ -441,13 +441,14 @@ static void ide_issue_trim_cb(void *opaque, int ret)
     }
 }
 
-BlockAIOCB *ide_issue_trim(BlockBackend *blk,
+BlockAIOCB *ide_issue_trim(
         int64_t sector_num, QEMUIOVector *qiov, int nb_sectors,
-        BlockCompletionFunc *cb, void *opaque)
+        BlockCompletionFunc *cb, void *cb_opaque, void *opaque)
 {
+    BlockBackend *blk = opaque;
     TrimAIOCB *iocb;
 
-    iocb = blk_aio_get(&trim_aiocb_info, blk, cb, opaque);
+    iocb = blk_aio_get(&trim_aiocb_info, blk, cb, cb_opaque);
     iocb->blk = blk;
     iocb->bh = qemu_bh_new(ide_trim_bh_cb, iocb);
     iocb->ret = 0;
@@ -869,8 +870,9 @@ static void ide_dma_cb(void *opaque, int ret)
                                            ide_dma_cb, s);
         break;
     case IDE_DMA_TRIM:
-        s->bus->dma->aiocb = dma_blk_io(s->blk, &s->sg, sector_num,
-                                        ide_issue_trim, ide_dma_cb, s,
+        s->bus->dma->aiocb = dma_blk_io(blk_get_aio_context(s->blk),
+                                        &s->sg, sector_num,
+                                        ide_issue_trim, s->blk, ide_dma_cb, s,
                                         DMA_DIRECTION_TO_DEVICE);
         break;
     default:
