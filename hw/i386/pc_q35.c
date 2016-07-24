29a28f5e5e029f91cd2bc671e1ba2add3dfddde3
Marcel Apfelbaum <marcel@redhat.com>
2016-07-24 16:29:00 +0200

    q35: allow dynamic sysbus
    RH-Author: Marcel Apfelbaum <marcel@redhat.com>
    Message-id: <1469377743-9714-11-git-send-email-marcel@redhat.com>
    Patchwork-id: 71377
    O-Subject: [RHEL-7.3 qemu-kvm-rhev PATCH 10/13] q35: allow dynamic sysbus
    Bugzilla: 1350196
    RH-Acked-by: Auger Eric <eric.auger@redhat.com>
    RH-Acked-by: Peter Xu <peterx@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    
    Allow adding sysbus devices with -device on Q35.
    
    At first Q35 will support only intel-iommu to be added this way,
    however the command line will support all sysbus devices.
    
    Mark with 'cannot_instantiate_with_device_add_yet' the ones
    causing immediate problems (e.g. crashes).
    
      Conflicts:
     	hw/pci-host/piix.c
            'cannot_instantiate_with_device_add_yet' was already set for RH
    
    Signed-off-by: Marcel Apfelbaum <marcel@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    (cherry picked from commit bf8d492405feaee2c1685b3b9d5e03228ed3e47f)
    Signed-off-by: Marcel Apfelbaum <marcel@redhat.com>
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -282,6 +282,7 @@ static void pc_q35_machine_options(MachineClass *m)
     m->default_machine_opts = "firmware=bios-256k.bin";
     m->default_display = "std";
     m->no_floppy = 1;
+    m->has_dynamic_sysbus = true;
 }
 
 static void pc_q35_2_6_machine_options(MachineClass *m)
