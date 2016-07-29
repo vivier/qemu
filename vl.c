d15566b6a91973dbf83c92a9678bd3ac4939af75
Miroslav Rezanina <mrezanin@redhat.com>
2016-07-29 07:41:27 +0200

    trace: remove malloc tracing
    RH-Author: Miroslav Rezanina <mrezanin@redhat.com>
    Message-id: <1469778087-316-1-git-send-email-mrezanin@redhat.com>
    Patchwork-id: 71514
    O-Subject: [RHEL-7.3 qemu-kvm PATCH] trace: remove malloc tracing
    Bugzilla: 1360137
    RH-Acked-by: Fam Zheng <famz@redhat.com>
    RH-Acked-by: Markus Armbruster <armbru@redhat.com>
    RH-Acked-by: Laszlo Ersek <lersek@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    
    From: Paolo Bonzini <pbonzini@redhat.com>
    
    The malloc vtable is not supported anymore in glib, because it broke
    when constructors called g_malloc.  Remove tracing of g_malloc,
    g_realloc and g_free calls.
    
    Note that, for systemtap users, glib also provides tracepoints
    glib.mem_alloc, glib.mem_free, glib.mem_realloc, glib.slice_alloc
    and glib.slice_free.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Alberto Garcia <berto@igalia.com>
    Message-id: 1442417924-25831-1-git-send-email-pbonzini@redhat.com
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    (cherry picked from commit 98cf48f60aa4999f5b2808569a193a401a390e6a)
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- vl.c
+++ vl.c
@@ -2738,26 +2738,6 @@ static const QEMUOption *lookup_opt(int argc, char **argv,
     return popt;
 }
 
-static gpointer malloc_and_trace(gsize n_bytes)
-{
-    void *ptr = malloc(n_bytes);
-    trace_g_malloc(n_bytes, ptr);
-    return ptr;
-}
-
-static gpointer realloc_and_trace(gpointer mem, gsize n_bytes)
-{
-    void *ptr = realloc(mem, n_bytes);
-    trace_g_realloc(mem, n_bytes, ptr);
-    return ptr;
-}
-
-static void free_and_trace(gpointer mem)
-{
-    trace_g_free(mem);
-    free(mem);
-}
-
 static int object_set_property(const char *name, const char *value, void *opaque)
 {
     Object *obj = OBJECT(opaque);
@@ -2832,11 +2812,6 @@ int main(int argc, char **argv, char **envp)
     bool userconfig = true;
     const char *log_mask = NULL;
     const char *log_file = NULL;
-    GMemVTable mem_trace = {
-        .malloc = malloc_and_trace,
-        .realloc = realloc_and_trace,
-        .free = free_and_trace,
-    };
     const char *trace_events = NULL;
     const char *trace_file = NULL;
     FILE *vmstate_dump_file = NULL;
@@ -2845,7 +2820,6 @@ int main(int argc, char **argv, char **envp)
     error_set_progname(argv[0]);
     qemu_init_exec_dir(argv[0]);
 
-    g_mem_set_vtable(&mem_trace);
     if (!g_thread_supported()) {
 #if !GLIB_CHECK_VERSION(2, 31, 0)
         g_thread_init(NULL);
