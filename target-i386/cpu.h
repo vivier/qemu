cf05052b04ebb27e6ab6c2edb52ab836d617461b
Igor Mammedov <imammedo@redhat.com>
2016-08-01 08:11:53 +0200

    pc: Parse CPU features only once
    RH-Author: Igor Mammedov <imammedo@redhat.com>
    Message-id: <1470039143-24450-49-git-send-email-imammedo@redhat.com>
    Patchwork-id: 71664
    O-Subject: [RHEV-7.3 qemu-kvm-rhev PATCH 48/78] pc: Parse CPU features only once
    Bugzilla: 1087672
    RH-Acked-by: Marcel Apfelbaum <marcel@redhat.com>
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    RH-Acked-by: Eduardo Habkost <ehabkost@redhat.com>
    
    Considering that features are converted to global properties and
    global properties are automatically applied to every new instance
    of created CPU (at object_new() time), there is no point in
    parsing cpu_model string every time a CPU created. So move
    parsing outside CPU creation loop and do it only once.
    
    Parsing also should be done before any CPU is created so that
    features would affect the first CPU a well.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    (cherry picked from commit 6aff24c6a61c6fec31e555c7748ba6085b7b2c06)
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- target-i386/cpu.h
+++ target-i386/cpu.h
@@ -1135,7 +1135,6 @@ void x86_cpu_exec_enter(CPUState *cpu);
 void x86_cpu_exec_exit(CPUState *cpu);
 
 X86CPU *cpu_x86_init(const char *cpu_model);
-X86CPU *cpu_x86_create(const char *cpu_model, Error **errp);
 void x86_cpu_list(FILE *f, fprintf_function cpu_fprintf);
 void x86_cpudef_setup(void);
 int cpu_x86_support_mca_broadcast(CPUX86State *env);
