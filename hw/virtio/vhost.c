9c41375f56d85fdc40dfb611ded04670c27ac087
Marc-André Lureau <marcandre.lureau@redhat.com>
2016-08-11 08:43:40 +0200

    vhost: add assert() to check runtime behaviour
    RH-Author: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-id: <20160811084348.10475-21-marcandre.lureau@redhat.com>
    Patchwork-id: 71936
    O-Subject: [RHEV-7.3 qemu-kvm-rhev PATCH 20/28] vhost: add assert() to check runtime behaviour
    Bugzilla: 1355902
    RH-Acked-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    RH-Acked-by: Victor Kaplansky <vkaplans@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    All these functions must be called only after the backend is connected.
    They are called from virtio-net.c, after either virtio or link status
    change.
    
    The check for nc->peer->link_down should ensure vhost_net_{start,stop}()
    are always called between vhost_user_{start,stop}().
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    (cherry picked from commit 8695de0fcf739b2071018d933b407ed8c16f40e8)
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1355902
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- hw/virtio/vhost.c
+++ hw/virtio/vhost.c
@@ -1245,6 +1245,9 @@ void vhost_virtqueue_mask(struct vhost_dev *hdev, VirtIODevice *vdev, int n,
     int r, index = n - hdev->vq_index;
     struct vhost_vring_file file;
 
+    /* should only be called after backend is connected */
+    assert(hdev->vhost_ops);
+
     if (mask) {
         assert(vdev->use_guest_notifier_mask);
         file.fd = event_notifier_get_fd(&hdev->vqs[index].masked_notifier);
@@ -1290,6 +1293,9 @@ void vhost_ack_features(struct vhost_dev *hdev, const int *feature_bits,
 int vhost_dev_start(struct vhost_dev *hdev, VirtIODevice *vdev)
 {
     int i, r;
+
+    /* should only be called after backend is connected */
+    assert(hdev->vhost_ops);
 
     hdev->started = true;
 
@@ -1352,6 +1358,9 @@ fail_features:
 void vhost_dev_stop(struct vhost_dev *hdev, VirtIODevice *vdev)
 {
     int i;
+
+    /* should only be called after backend is connected */
+    assert(hdev->vhost_ops);
 
     for (i = 0; i < hdev->nvqs; ++i) {
         vhost_virtqueue_stop(hdev,
