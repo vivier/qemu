cc13b6ce989f1cbea6671b52286866de9714d697
Peter Xu <peterx@redhat.com>
2016-08-18 03:00:44 +0200

    kvm-irqchip: i386: add hook for add/remove virq
    RH-Author: Peter Xu <peterx@redhat.com>
    Message-id: <1471489253-2811-26-git-send-email-peterx@redhat.com>
    Patchwork-id: 72010
    O-Subject: [RHEL-7.3 qemu-kvm-rhev v2 25/34] kvm-irqchip: i386: add hook for add/remove virq
    Bugzilla: 1358653
    RH-Acked-by: Marcel Apfelbaum <marcel@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    
    Adding two hooks to be notified when adding/removing msi routes. There
    are two kinds of MSI routes:
    
    - in kvm_irqchip_add_irq_route(): before assigning IRQFD. Used by
      vhost, vfio, etc.
    
    - in kvm_irqchip_send_msi(): when sending direct MSI message, if
      direct MSI not allowed, we will first create one MSI route entry
      in the kernel, then trigger it.
    
    This patch only hooks the first one (irqfd case). We do not need to
    take care for the 2nd one, since it's only used by QEMU userspace
    (kvm-apic) and the messages will always do in-time translation when
    triggered. While we need to note them down for the 1st one, so that we
    can notify the kernel when cache invalidation happens.
    
    Also, we do not hook IOAPIC msi routes (we have explicit notifier for
    IOAPIC to keep its cache updated). We only need to care about irqfd
    users.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    (cherry picked from commit 38d87493f37e5dc442c7419c5843fcf60bb39d63)
    Signed-off-by: Peter Xu <peterx@redhat.com>
    
    Moving splitted trace-events into big trace-event file.
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- kvm-all.c
+++ kvm-all.c
@@ -1127,6 +1127,7 @@ void kvm_irqchip_release_virq(KVMState *s, int virq)
         }
     }
     clear_gsi(s, virq);
+    kvm_arch_release_virq_post(virq);
 }
 
 static unsigned int kvm_hash_msi(uint32_t data)
@@ -1275,6 +1276,7 @@ int kvm_irqchip_add_msi_route(KVMState *s, int vector, PCIDevice *dev)
     }
 
     kvm_add_routing_entry(s, &kroute);
+    kvm_arch_add_msi_route_post(&kroute, vector, dev);
     kvm_irqchip_commit_routes(s);
 
     return virq;
