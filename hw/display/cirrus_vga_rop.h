d27fae125c1efd59ba3263260d41f8e054b070a2
Gerd Hoffmann <kraxel@redhat.com>
2017-03-21 09:58:01 +0100

    fix :cirrus_vga fix OOB read case qemu Segmentation fault
    RH-Author: Gerd Hoffmann <kraxel@redhat.com>
    Message-id: <1490090287-1503-2-git-send-email-kraxel@redhat.com>
    Patchwork-id: 74419
    O-Subject: [RHEL-7.4 qemu-kvm PATCH 1/7] fix :cirrus_vga fix OOB read case qemu Segmentation fault
    Bugzilla: 1430059
    RH-Acked-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    From: hangaohuai <hangaohuai@huawei.com>
    
    check the validity of parameters in cirrus_bitblt_rop_fwd_transp_xxx
    and cirrus_bitblt_rop_fwd_xxx to avoid the OOB read which causes qemu Segmentation fault.
    
    After the fix, we will touch the assert in
    cirrus_invalidate_region:
    assert(off_cur_end >= off_cur);
    
    Signed-off-by: fangying <fangying1@huawei.com>
    Signed-off-by: hangaohuai <hangaohuai@huawei.com>
    Message-id: 20170314063919.16200-1-hangaohuai@huawei.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    (cherry picked from commit 215902d7b6fb50c6fc216fc74f770858278ed904)
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- hw/display/cirrus_vga_rop.h
+++ hw/display/cirrus_vga_rop.h
@@ -98,6 +98,11 @@ glue(glue(cirrus_bitblt_rop_fwd_transp_, ROP_NAME),_8)(CirrusVGAState *s,
     uint8_t p;
     dstpitch -= bltwidth;
     srcpitch -= bltwidth;
+
+    if (bltheight > 1 && (dstpitch < 0 || srcpitch < 0)) {
+        return;
+    }
+
     for (y = 0; y < bltheight; y++) {
         for (x = 0; x < bltwidth; x++) {
 	    p = *dst;
@@ -144,6 +149,11 @@ glue(glue(cirrus_bitblt_rop_fwd_transp_, ROP_NAME),_16)(CirrusVGAState *s,
     uint8_t p1, p2;
     dstpitch -= bltwidth;
     srcpitch -= bltwidth;
+
+    if (bltheight > 1 && (dstpitch < 0 || srcpitch < 0)) {
+        return;
+    }
+
     for (y = 0; y < bltheight; y++) {
         for (x = 0; x < bltwidth; x+=2) {
 	    p1 = *dst;
