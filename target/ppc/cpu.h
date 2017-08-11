ad017413736e51417a4b67e965ad1e968518a957
David Gibson <dgibson@redhat.com>
2017-08-11 06:09:09 +0200

    target/ppc: Add stub implementation of the PSSCR
    RH-Author: David Gibson <dgibson@redhat.com>
    Message-id: <20170811060909.31369-3-dgibson@redhat.com>
    Patchwork-id: 75934
    O-Subject: [RHEL-7.4-ALT qemu-kvm PATCH 2/2] target/ppc: Add stub implementation of the PSSCR
    Bugzilla: 1478822
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    From: David Gibson <david@gibson.dropbear.id.au>
    
    The PSSCR register added in POWER9 controls certain power saving mode
    behaviours.  Mostly, it's not relevant to TCG, however because qemu
    doesn't know about it yet, it doesn't synchronize the state with KVM,
    and thus it doesn't get migrated.
    
    To fix that, this adds a minimal stub implementation of the register.
    This isn't complete, even to the extent that an implementation is
    possible in TCG, just enough to get migration working.  We need to
    come back later and at least properly filter the various fields in the
    register based on privilege level.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    (cherry picked from commit b8af5b2d5f67b0e1b274f8532f42a47bfe46ea3b)
    
    Signed-off-by: David Gibson <dgibson@redhat.com>
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- target/ppc/cpu.h
+++ target/ppc/cpu.h
@@ -1764,6 +1764,7 @@ int ppc_compat_max_threads(PowerPCCPU *cpu);
 #define SPR_IC                (0x350)
 #define SPR_VTB               (0x351)
 #define SPR_MMCRC             (0x353)
+#define SPR_PSSCR             (0x357)
 #define SPR_440_INV0          (0x370)
 #define SPR_440_INV1          (0x371)
 #define SPR_440_INV2          (0x372)
