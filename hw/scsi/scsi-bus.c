vivier/qemu

commit 7977e603169c92da4b0ecd656c3346500a93897b

scsi: Disable deprecated implicit SCSI HBA creation more cleanly
RH-Author: Markus Armbruster <armbru@redhat.com>
Message-id: <1493382147-23057-2-git-send-email-armbru@redhat.com>
Patchwork-id: 74946
O-Subject: [RHV-7.4 qemu-kvm-rhev PATCH] scsi: Disable deprecated implicit SCSI HBA creation more cleanly
Bugzilla: 971799
RH-Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
RH-Acked-by: John Snow <jsnow@redhat.com>
RH-Acked-by: Jeffrey Cody <jcody@redhat.com>

The PC machines (pc-q35-* pc-i440fx-* pc-* isapc xenfv) automatically
create lsi53c895a SCSI HBAs and SCSI devices to honor -drive if=scsi.
Deprecated upstream since commit f778a82, v2.9.0, not supported at all
in RHEL.  The way it fails is rather ugly, though:

    $ qemu-kvm -nodefaults -S -drive if=scsi,media=cdrom
    qemu-kvm: Unknown device 'lsi53c895a' for bus 'PCI'
    Aborted (core dumped)

Recent upstream work permit us to make this fail cleanly by commenting
out the code that tries to create lsi53c895a SCSI HBAs in
pc_pci_device_init():

    qemu-kvm: -drive if=scsi,media=cdrom: machine type does not support if=scsi,bus=0,unit=0

However, by itself this would make another deprecated feature
available in RHEL: drives defined with if=scsi get picked up by SCSI
HBAs added with -device, unlike other interface types.  Deprecated
upstream since commit a64aa57, v2.9.0.  Comment out that code, too.
Bonus: that code can be rather slow with a large number of drives, so
good riddance.

Signed-off-by: Markus Armbruster <armbru@redhat.com>
Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

hw/scsi/scsi-bus.c
@@ -265,6 +265,8 @@ SCSIDevice *scsi_bus_legacy_add_drive(SCSIBus *bus, BlockBackend *blk,
 
 void scsi_bus_legacy_handle_cmdline(SCSIBus *bus, bool deprecated)
 {
+#if 0 /* Disabled for Red Hat Enterprise Linux */
+
     Location loc;
     DriveInfo *dinfo;
     int unit;
@@ -291,7 +293,10 @@ void scsi_bus_legacy_handle_cmdline(SCSIBus *bus, bool deprecated)
                                   unit, false, -1, NULL, &error_fatal);
     }
     loc_pop(&loc);
+#endif
 }
+
+#if 0 /* Disabled for Red Hat Enterprise Linux */
 
 static bool is_scsi_hba_with_legacy_magic(Object *obj)
 {
@@ -328,6 +333,8 @@ void scsi_legacy_handle_cmdline(void)
     object_child_foreach_recursive(object_get_root(),
                                    scsi_legacy_handle_cmdline_cb, NULL);
 }
+
+#endif
 
 static int32_t scsi_invalid_field(SCSIRequest *req, uint8_t *buf)
 {

hw/i386/pc.c
@@ -1648,6 +1648,7 @@ void pc_nic_init(ISABus *isa_bus, PCIBus *pci_bus)
 
 void pc_pci_device_init(PCIBus *pci_bus)
 {
+#if 0 /* Disabled for Red Hat Enterprise Linux */
     int max_bus;
     int bus;
 
@@ -1661,6 +1662,7 @@ void pc_pci_device_init(PCIBus *pci_bus)
          * this usage is deprecated.
          */
     }
+#endif
 }
 
 void ioapic_init_gsi(GSIState *gsi_state, const char *parent_name)

vl.c
@@ -4602,6 +4602,7 @@ int main(int argc, char **argv, char **envp)
 
     rom_reset_order_override();
 
+#if 0 /* Disabled for Red Hat Enterprise Linux */
     /*
      * Create frontends for -drive if=scsi leftovers.
      * Normally, frontends for -drive get created by machine
@@ -4610,6 +4611,7 @@ int main(int argc, char **argv, char **envp)
      * implementation accident, and deprecated.
      */
     scsi_legacy_handle_cmdline();
+#endif
 
     /* Did we create any drives that we failed to create a device for? */
     drive_check_orphaned();

commit 8198d8d36fed85743c4d2b26aa1670f5bc3a9793

scsi: Fix onboard HBAs to pick up -drive if=scsi
RH-Author: Markus Armbruster <armbru@redhat.com>
Message-id: <20171128132417.18247-2-armbru@redhat.com>
Patchwork-id: 77934
O-Subject: [RHV-7.5 qemu-kvm-rhev PATCH v2 1/1] scsi: Fix onboard HBAs to pick up -drive if=scsi
Bugzilla: 1497740
RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
RH-Acked-by: Thomas Huth <thuth@redhat.com>
RH-Acked-by: Jeffrey Cody <jcody@redhat.com>

Downstream commit 7977e6031 "scsi: Disable deprecated implicit SCSI
HBA creation more cleanly" regressed -drive if=scsi and its sugared
forms like -cdrom with onboard HBAs.  Instead of creating SCSI devices
connected to the onboard HBA, we reject these options like this:

    $ qemu-kvm -cdrom r7.iso
    qemu-kvm: -cdrom r7.iso: machine type does not support if=scsi,bus=0,unit=2

The culprit is scsi_bus_legacy_handle_cmdline(): commit 7977e6031
accidentally neutered it for the non-deprecated case (onboard SCSI
HBA) in addition to the deprecated case (non-onboard SCSI HBA with
legacy magic).  Fix by narrowing the neutering to the deprecated case.

Signed-off-by: Markus Armbruster <armbru@redhat.com>
Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

hw/scsi/scsi-bus.c
@@ -265,8 +265,6 @@ SCSIDevice *scsi_bus_legacy_add_drive(SCSIBus *bus, BlockBackend *blk,
 
 void scsi_bus_legacy_handle_cmdline(SCSIBus *bus, bool deprecated)
 {
-#if 0 /* Disabled for Red Hat Enterprise Linux */
-
     Location loc;
     DriveInfo *dinfo;
     int unit;
@@ -279,6 +277,7 @@ void scsi_bus_legacy_handle_cmdline(SCSIBus *bus, bool deprecated)
         }
         qemu_opts_loc_restore(dinfo->opts);
         if (deprecated) {
+#if 0 /* Disabled for Red Hat Enterprise Linux */
             /* Handling -drive not claimed by machine initialization */
             if (blk_get_attached_dev(blk_by_legacy_dinfo(dinfo))) {
                 continue;       /* claimed */
@@ -288,12 +287,14 @@ void scsi_bus_legacy_handle_cmdline(SCSIBus *bus, bool deprecated)
                             " machine type",
                             bus->busnr, unit);
             }
+#else
+            continue;
+#endif
         }
         scsi_bus_legacy_add_drive(bus, blk_by_legacy_dinfo(dinfo),
                                   unit, false, -1, NULL, &error_fatal);
     }
     loc_pop(&loc);
-#endif
 }
 
 #if 0 /* Disabled for Red Hat Enterprise Linux */