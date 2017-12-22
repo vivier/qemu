09d5b181e01310e90c49c862a4ae6822eff9e873
Eduardo Habkost <ehabkost@redhat.com>
2017-12-22 17:51:24 +0100

    target-i386: cpu: add new CPU models for indirect branch predictor restrictions
    RH-Author: Eduardo Habkost <ehabkost@redhat.com>
    Message-id: <20171213174257.20475-4-ehabkost@redhat.com>
    Patchwork-id: n/a
    O-Subject: [CONFIDENTIAL][RHEL7.3.z qemu-kvm PATCH 3/3] target-i386: cpu: add new CPU models for indirect branch predictor restrictions
    Bugzilla: 1526981
    CVE: CVE-2017-5715
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    RH-Acked-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    
    From: Eduardo Habkost <ehabkost@redhat.com>
    
    To ensure the New CPU models won't introduce any unexpected
    changes except for the spec-ctrl feature (even if people are
    running older machine-types), copy all compat_props entries for
    existing CPU models to their *-IBRS versions.
    
    The only entries that are not being copied are the ones touching
    "(min-)level" and "(min-)xlevel" because it's an expected result
    of the CPU model change (otherwise the spec-ctrl feature would
    remain unavailable to the guest).
    
    The entries that had to be copied can be found using:
      $ git grep -E 'Nehalem|Westmere|SandyBridge|IvyBridge|Haswell-noTSX|Haswell|Broadwell-noTSX|Broadwell|Skylake-Client|Skylake-Server|EPYC'
    
    Note that the upstream-only PC_COMPAT_* macros are not being
    touched as they are not used by the RHEL machine-types.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target-i386/cpu.h
+++ target-i386/cpu.h
@@ -583,6 +583,10 @@ typedef uint32_t FeatureWordArray[FEATURE_WORDS];
 #define CPUID_7_0_ECX_OSPKE    (1U << 4)
 #define CPUID_7_0_ECX_RDPID    (1U << 22)
 
+#define CPUID_7_0_EDX_SPEC_CTRL     (1U << 26) /* Indirect Branch - Restrict Speculation */
+
+#define CPUID_8000_0008_EBX_IBPB    (1U << 12) /* Indirect Branch Prediction Barrier */
+
 #define CPUID_XSAVE_XSAVEOPT   (1U << 0)
 #define CPUID_XSAVE_XSAVEC     (1U << 1)
 #define CPUID_XSAVE_XGETBV1    (1U << 2)
