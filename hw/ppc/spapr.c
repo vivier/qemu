268880a2fee41c5c1929aac996a3292db41233cb
David Gibson <dgibson@redhat.com>
2018-01-19 02:34:39 +0100

    spapr: Handle VMX/VSX presence as an spapr capability flag
    RH-Author: David Gibson <dgibson@redhat.com>
    Message-id: <20180119023442.28577-5-dgibson@redhat.com>
    Patchwork-id: 78671
    O-Subject: [RHEL-7.5 qemu-kvm-rhev PATCH 4/7] spapr: Handle VMX/VSX presence as an spapr capability flag
    Bugzilla: 1523414
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    From: David Gibson <david@gibson.dropbear.id.au>
    
    We currently have some conditionals in the spapr device tree code to decide
    whether or not to advertise the availability of the VMX (aka Altivec) and
    VSX vector extensions to the guest, based on whether the guest cpu has
    those features.
    
    This can lead to confusion and subtle failures on migration, since it makes
    a guest visible change based only on host capabilities.  We now have a
    better mechanism for this, in spapr capabilities flags, which explicitly
    depend on user options rather than host capabilities.
    
    Rework the advertisement of VSX and VMX based on a new VSX capability.  We
    no longer bother with a conditional for VMX support, because every CPU
    that's ever been supported by the pseries machine type supports VMX.
    
    NOTE: Some userspace distributions (e.g. RHEL7.4) already rely on
    availability of VSX in libc, so using cap-vsx=off may lead to a fatal
    SIGILL in init.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    (cherry picked from commit 2938664286499c0c30d6e455a7e2e5d3e6c3f63d)
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    Conflicts:
    	hw/ppc/spapr.c
    
    Because of the difference between upstream and downstream machine type
    versions.  Adjusted the logic to enable CAP_VSX on all downstream
    machine types, as it is for all upstream types.
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1523414
    
    Signed-off-by: David Gibson <dgibson@redhat.com>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -560,14 +560,16 @@ static void spapr_populate_cpu_dt(CPUState *cs, void *fdt, int offset,
                           segs, sizeof(segs))));
     }
 
-    /* Advertise VMX/VSX (vector extensions) if available
-     *   0 / no property == no vector extensions
+    /* Advertise VSX (vector extensions) if available
      *   1               == VMX / Altivec available
-     *   2               == VSX available */
-    if (env->insns_flags & PPC_ALTIVEC) {
-        uint32_t vmx = (env->insns_flags2 & PPC2_VSX) ? 2 : 1;
-
-        _FDT((fdt_setprop_cell(fdt, offset, "ibm,vmx", vmx)));
+     *   2               == VSX available
+     *
+     * Only CPUs for which we create core types in spapr_cpu_core.c
+     * are possible, and all of those have VMX */
+    if (spapr_has_cap(spapr, SPAPR_CAP_VSX)) {
+        _FDT((fdt_setprop_cell(fdt, offset, "ibm,vmx", 2)));
+    } else {
+        _FDT((fdt_setprop_cell(fdt, offset, "ibm,vmx", 1)));
     }
 
     /* Advertise DFP (Decimal Floating Point) if available
@@ -3648,7 +3650,7 @@ static void spapr_machine_class_init(ObjectClass *oc, void *data)
     mc->numa_mem_align_shift = 28;
     smc->has_power9_support = true;
 
-    smc->default_caps = spapr_caps(0);
+    smc->default_caps = spapr_caps(SPAPR_CAP_VSX);
     spapr_caps_add_properties(smc, &error_abort);
 }
 
@@ -4054,7 +4056,7 @@ static void spapr_machine_rhel740_class_options(MachineClass *mc)
     smc->has_power9_support = false;
     smc->pre_2_10_has_unused_icps = true;
     smc->resize_hpt_default = SPAPR_RESIZE_HPT_DISABLED;
-    smc->default_caps = spapr_caps(SPAPR_CAP_HTM);
+    smc->default_caps = spapr_caps(SPAPR_CAP_HTM | SPAPR_CAP_VSX);
 }
 
 DEFINE_SPAPR_MACHINE(rhel740, "rhel7.4.0", false);
