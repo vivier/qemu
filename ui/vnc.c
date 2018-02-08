3b0e5204ae4a681ed9f6bedd3cd18ecad877546c
Daniel P. Berrange <berrange@redhat.com>
2018-02-08 17:50:35 +0100

    ui: refactor code for determining if an update should be sent to the client
    RH-Author: Daniel P. Berrange <berrange@redhat.com>
    Message-id: <20180208175041.5634-22-berrange@redhat.com>
    Patchwork-id: 78954
    O-Subject: [RHEL-7.5 qemu-kvm PATCH v1 21/27] ui: refactor code for determining if an update should be sent to the client
    Bugzilla: 1527405
    RH-Acked-by: Laszlo Ersek <lersek@redhat.com>
    RH-Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    From: "Daniel P. Berrange" <berrange@redhat.com>
    
    The logic for determining if it is possible to send an update to the client
    will become more complicated shortly, so pull it out into a separate method
    for easier extension later.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-id: 20171218191228.31018-9-berrange@redhat.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    (cherry picked from commit 0bad834228b9ee63e4239108d02dcb94568254d0)
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- ui/vnc.c
+++ ui/vnc.c
@@ -846,6 +846,25 @@ static int find_and_clear_dirty_height(struct VncState *vs,
     return h;
 }
 
+static bool vnc_should_update(VncState *vs)
+{
+    switch (vs->update) {
+    case VNC_STATE_UPDATE_NONE:
+        break;
+    case VNC_STATE_UPDATE_INCREMENTAL:
+        /* Only allow incremental updates if the output buffer
+         * is empty, or if audio capture is enabled.
+         */
+        if (!vs->output.offset || vs->audio_cap) {
+            return true;
+        }
+        break;
+    case VNC_STATE_UPDATE_FORCE:
+        return true;
+    }
+    return false;
+}
+
 static int vnc_update_client(VncState *vs, int has_dirty)
 {
     VncDisplay *vd = vs->vd;
@@ -860,13 +879,7 @@ static int vnc_update_client(VncState *vs, int has_dirty)
     }
 
     vs->has_dirty += has_dirty;
-    if (vs->update == VNC_STATE_UPDATE_NONE) {
-        return 0;
-    }
-
-    if (vs->output.offset && !vs->audio_cap &&
-        vs->update != VNC_STATE_UPDATE_FORCE) {
-        /* kernel send buffers are full -> drop frames to throttle */
+    if (!vnc_should_update(vs)) {
         return 0;
     }
 
