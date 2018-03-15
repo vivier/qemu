fa6c9d848a2628a42f5b9c938454cd2e690c44c3
Suraj Jitindar Singh <sursingh@redhat.com>
2018-03-15 02:37:50 +0100

    linux-headers: update
    RH-Author: Suraj Jitindar Singh <sursingh@redhat.com>
    Message-id: <1521081482-24429-12-git-send-email-sursingh@redhat.com>
    Patchwork-id: 79318
    O-Subject: [RHEL7.4.z qemu-kvm-rhev PATCH 11/23] linux-headers: update
    Bugzilla: 1544741
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    From: Cornelia Huck <cohuck@redhat.com>
    
    Update headers against 4.15-rc9.
    
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>
    (cherry picked from commit 9cbb636270b4df6f0a548e5c34b895330db5df8b)
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    Conflicts:
    	include/standard-headers/asm-s390/virtio-ccw.h
    	linux-headers/linux/kvm.h
    	Minor massage required due to previous header updates missing
    
    Signed-off-by: Suraj Jitindar Singh <sursingh@redhat.com>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -600,9 +600,9 @@ struct kvm_s390_irq {
 
 struct kvm_s390_irq_state {
 	__u64 buf;
-	__u32 flags;
+	__u32 flags;        /* will stay unused for compatibility reasons */
 	__u32 len;
-	__u32 reserved[4];
+	__u32 reserved[4];  /* will stay unused for compatibility reasons */
 };
 
 /* for KVM_SET_GUEST_DEBUG */
@@ -895,6 +895,8 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_SPAPR_TCE_VFIO 142
 #define KVM_CAP_X86_GUEST_MWAIT 143
 #define KVM_CAP_ARM_USER_IRQ 144
+#define KVM_CAP_PPC_GET_CPU_CHAR 151
+#define KVM_CAP_S390_BPB 152
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
@@ -1224,6 +1226,8 @@ struct kvm_s390_ucas_mapping {
 #define KVM_PPC_CONFIGURE_V3_MMU  _IOW(KVMIO,  0xaf, struct kvm_ppc_mmuv3_cfg)
 /* Available with KVM_CAP_PPC_RADIX_MMU */
 #define KVM_PPC_GET_RMMU_INFO	  _IOW(KVMIO,  0xb0, struct kvm_ppc_rmmu_info)
+/* Available with KVM_CAP_PPC_GET_CPU_CHAR */
+#define KVM_PPC_GET_CPU_CHAR	  _IOR(KVMIO,  0xb1, struct kvm_ppc_cpu_char)
 
 /* ioctl for vm fd */
 #define KVM_CREATE_DEVICE	  _IOWR(KVMIO,  0xe0, struct kvm_create_device)
