7c2d577b80ebfcb765594d18e7f807201f905f11
Suraj Jitindar Singh <sursingh@redhat.com>
2018-03-15 02:37:43 +0100

    target/ppc/kvm: Add cap_ppc_safe_[cache/bounds_check/indirect_branch]
    RH-Author: Suraj Jitindar Singh <sursingh@redhat.com>
    Message-id: <1521081482-24429-5-git-send-email-sursingh@redhat.com>
    Patchwork-id: 79300
    O-Subject: [RHEL7.4.z qemu-kvm-rhev PATCH 04/23] target/ppc/kvm: Add cap_ppc_safe_[cache/bounds_check/indirect_branch]
    Bugzilla: 1544741
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    From: Suraj Jitindar Singh <sjitindarsingh@gmail.com>
    
    Add three new kvm capabilities used to represent the level of host support
    for three corresponding workarounds.
    
    Host support for each of the capabilities is queried through the
    new ioctl KVM_PPC_GET_CPU_CHAR which returns four uint64 quantities. The
    first two, character and behaviour, represent the available
    characteristics of the cpu and the behaviour of the cpu respectively.
    The second two, c_mask and b_mask, represent the mask of known bits for
    the character and beheviour dwords respectively.
    
    Signed-off-by: Suraj Jitindar Singh <sjitindarsingh@gmail.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    [dwg: Correct some compile errors due to name change in final kernel
     patch version]
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    Conflicts:
    	target/ppc/kvm.c
    	Minor massage require due to lack of hpt-resize code
    
    Signed-off-by: Suraj Jitindar Singh <sursingh@redhat.com>
    (cherry picked from commit 8acc2ae5e91681ceda3ff4cf946ebf163f6012e9)

--- target/ppc/kvm.c
+++ target/ppc/kvm.c
@@ -87,6 +87,9 @@ static int cap_fixup_hcalls;
 static int cap_htm;             /* Hardware transactional memory support */
 static int cap_mmu_radix;
 static int cap_mmu_hash_v3;
+static int cap_ppc_safe_cache;
+static int cap_ppc_safe_bounds_check;
+static int cap_ppc_safe_indirect_branch;
 
 static uint32_t debug_inst_opcode;
 
@@ -119,6 +122,7 @@ static bool kvmppc_is_pr(KVMState *ks)
 }
 
 static int kvm_ppc_register_host_cpu_type(void);
+static void kvmppc_get_cpu_characteristics(KVMState *s);
 
 int kvm_arch_init(MachineState *ms, KVMState *s)
 {
@@ -143,6 +147,7 @@ int kvm_arch_init(MachineState *ms, KVMState *s)
     cap_htm = kvm_vm_check_extension(s, KVM_CAP_PPC_HTM);
     cap_mmu_radix = kvm_vm_check_extension(s, KVM_CAP_PPC_MMU_RADIX);
     cap_mmu_hash_v3 = kvm_vm_check_extension(s, KVM_CAP_PPC_MMU_HASH_V3);
+    kvmppc_get_cpu_characteristics(s);
 
     if (!cap_interrupt_level) {
         fprintf(stderr, "KVM: Couldn't find level irq capability. Expect the "
@@ -2425,6 +2430,59 @@ static PowerPCCPUClass *ppc_cpu_get_family_class(PowerPCCPUClass *pcc)
     assert(oc);
 
     return POWERPC_CPU_CLASS(oc);
+}
+
+static void kvmppc_get_cpu_characteristics(KVMState *s)
+{
+    struct kvm_ppc_cpu_char c;
+    int ret;
+
+    /* Assume broken */
+    cap_ppc_safe_cache = 0;
+    cap_ppc_safe_bounds_check = 0;
+    cap_ppc_safe_indirect_branch = 0;
+
+    ret = kvm_vm_check_extension(s, KVM_CAP_PPC_GET_CPU_CHAR);
+    if (!ret) {
+        return;
+    }
+    ret = kvm_vm_ioctl(s, KVM_PPC_GET_CPU_CHAR, &c);
+    if (ret < 0) {
+        return;
+    }
+    /* Parse and set cap_ppc_safe_cache */
+    if (~c.behaviour & c.behaviour_mask & H_CPU_BEHAV_L1D_FLUSH_PR) {
+        cap_ppc_safe_cache = 2;
+    } else if ((c.character & c.character_mask & H_CPU_CHAR_L1D_THREAD_PRIV) &&
+               (c.character & c.character_mask
+                & (H_CPU_CHAR_L1D_FLUSH_ORI30 | H_CPU_CHAR_L1D_FLUSH_TRIG2))) {
+        cap_ppc_safe_cache = 1;
+    }
+    /* Parse and set cap_ppc_safe_bounds_check */
+    if (~c.behaviour & c.behaviour_mask & H_CPU_BEHAV_BNDS_CHK_SPEC_BAR) {
+        cap_ppc_safe_bounds_check = 2;
+    } else if (c.character & c.character_mask & H_CPU_CHAR_SPEC_BAR_ORI31) {
+        cap_ppc_safe_bounds_check = 1;
+    }
+    /* Parse and set cap_ppc_safe_indirect_branch */
+    if (c.character & H_CPU_CHAR_BCCTRL_SERIALISED) {
+        cap_ppc_safe_indirect_branch = 2;
+    }
+}
+
+int kvmppc_get_cap_safe_cache(void)
+{
+    return cap_ppc_safe_cache;
+}
+
+int kvmppc_get_cap_safe_bounds_check(void)
+{
+    return cap_ppc_safe_bounds_check;
+}
+
+int kvmppc_get_cap_safe_indirect_branch(void)
+{
+    return cap_ppc_safe_indirect_branch;
 }
 
 PowerPCCPUClass *kvm_ppc_get_host_cpu_class(void)
