10aec5afb21e388f153374b0b5f6242bffb4dea4
Laurent Vivier <laurent@vivier.eu>
2018-05-28 20:49:45 +0200

    linux-user: add pseudo /proc/cpuinfo for sparc
    SPARC libc6 debian package wants to check the cpu level to be
    installed or not:
    
      WARNING: This machine has a SPARC V8 or earlier class processor.
      Debian lenny and later does not support such old hardware
      any longer.
    
    To avoid this, they only need to know if the machine type is sun4u or sun4v,
    for that they read the information from /proc/cpuinfo.
    
    Fixes: 9a93c152fcdb4ab2cd85094487b33578fd693915
           ("linux-user: fix UNAME_MACHINE for sparc/sparc64")
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -7745,12 +7745,15 @@ static int is_proc_myself(const char *filename, const char *entry)
     return 0;
 }
 
-#if defined(HOST_WORDS_BIGENDIAN) != defined(TARGET_WORDS_BIGENDIAN)
+#if defined(HOST_WORDS_BIGENDIAN) != defined(TARGET_WORDS_BIGENDIAN) || \
+    defined(TARGET_SPARC)
 static int is_proc(const char *filename, const char *entry)
 {
     return strcmp(filename, entry) == 0;
 }
+#endif
 
+#if defined(HOST_WORDS_BIGENDIAN) != defined(TARGET_WORDS_BIGENDIAN)
 static int open_net_route(void *cpu_env, int fd)
 {
     FILE *fp;
@@ -7789,6 +7792,14 @@ static int open_net_route(void *cpu_env, int fd)
 }
 #endif
 
+#if defined(TARGET_SPARC)
+static int open_cpuinfo(void *cpu_env, int fd)
+{
+    dprintf(fd, "type\t\t: sun4u\n");
+    return 0;
+}
+#endif
+
 static int do_openat(void *cpu_env, int dirfd, const char *pathname, int flags, mode_t mode)
 {
     struct fake_open {
@@ -7804,6 +7815,9 @@ static int do_openat(void *cpu_env, int dirfd, const char *pathname, int flags, 
         { "cmdline", open_self_cmdline, is_proc_myself },
 #if defined(HOST_WORDS_BIGENDIAN) != defined(TARGET_WORDS_BIGENDIAN)
         { "/proc/net/route", open_net_route, is_proc },
+#endif
+#if defined(TARGET_SPARC)
+        { "/proc/cpuinfo", open_cpuinfo, is_proc },
 #endif
         { NULL, NULL, NULL }
     };
