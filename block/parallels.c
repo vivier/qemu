8a66fde222f0192c1d9ce215f297964ab34235c7
Markus Armbruster <armbru@redhat.com>
2018-06-18 08:43:20 +0200

    block: Factor out qobject_input_visitor_new_flat_confused()
    RH-Author: Markus Armbruster <armbru@redhat.com>
    Message-id: <20180618084330.30009-14-armbru@redhat.com>
    Patchwork-id: 80728
    O-Subject: [RHEL-7.6 qemu-kvm-rhev PATCH 13/23] block: Factor out qobject_input_visitor_new_flat_confused()
    Bugzilla: 1557995
    RH-Acked-by: Max Reitz <mreitz@redhat.com>
    RH-Acked-by: Jeffrey Cody <jcody@redhat.com>
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Kevin Wolf <kwolf@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit af91062ee1408f7f5bb58389d355d29a5040c648)
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- block/parallels.c
+++ block/parallels.c
@@ -615,7 +615,6 @@ static int coroutine_fn parallels_co_create_opts(const char *filename,
     Error *local_err = NULL;
     BlockDriverState *bs = NULL;
     QDict *qdict;
-    QObject *qobj;
     Visitor *v;
     int ret;
 
@@ -651,14 +650,12 @@ static int coroutine_fn parallels_co_create_opts(const char *filename,
     qdict_put_str(qdict, "driver", "parallels");
     qdict_put_str(qdict, "file", bs->node_name);
 
-    qobj = qdict_crumple_for_keyval_qiv(qdict, errp);
-    if (!qobj) {
+    v = qobject_input_visitor_new_flat_confused(qdict, errp);
+    if (!v) {
         ret = -EINVAL;
         goto done;
     }
 
-    v = qobject_input_visitor_new_keyval(qobj);
-    qobject_unref(qobj);
     visit_type_BlockdevCreateOptions(v, NULL, &create_options, &local_err);
     visit_free(v);
 
