df989559119707094b17269d025bcdf83df765f1
Tarun Gupta <tgupta@redhat.com>
2018-06-20 18:54:17 +0200

    ui/pixman: add qemu_drm_format_to_pixman()
    RH-Author: Tarun Gupta <tgupta@redhat.com>
    Message-id: <1529520865-18127-4-git-send-email-tgupta@redhat.com>
    Patchwork-id: 80911
    O-Subject: [RHEL7.6 qemu-kvm PATCH v3 03/11] ui/pixman: add qemu_drm_format_to_pixman()
    Bugzilla: 1555246
    RH-Acked-by: Alex Williamson <alex.williamson@redhat.com>
    RH-Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    Map drm fourcc codes to pixman formats.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed by: Kirti Wankhede <kwankhede@nvidia.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    
    (cherry picked from a5127bd73f77b90b50d63014be10cef467c1c3f9)
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- include/ui/qemu-pixman.h
+++ include/ui/qemu-pixman.h
@@ -27,9 +27,13 @@
 
 #ifdef HOST_WORDS_BIGENDIAN
 # define PIXMAN_BE_r8g8b8     PIXMAN_r8g8b8
+# define PIXMAN_LE_r8g8b8     PIXMAN_b8g8r8
+# define PIXMAN_LE_a8r8g8b8   PIXMAN_b8g8r8a8
 # define PIXMAN_LE_x8r8g8b8   PIXMAN_b8g8r8x8
 #else
 # define PIXMAN_BE_r8g8b8     PIXMAN_b8g8r8
+# define PIXMAN_LE_r8g8b8     PIXMAN_r8g8b8
+# define PIXMAN_LE_a8r8g8b8   PIXMAN_a8r8g8b8
 # define PIXMAN_LE_x8r8g8b8   PIXMAN_x8r8g8b8
 #endif
 
@@ -45,6 +49,8 @@ void qemu_pixman_linebuf_fill(pixman_image_t *linebuf, pixman_image_t *fb,
 pixman_image_t *qemu_pixman_mirror_create(pixman_format_code_t format,
                                           pixman_image_t *image);
 void qemu_pixman_image_unref(pixman_image_t *image);
+
+pixman_format_code_t qemu_drm_format_to_pixman(uint32_t drm_format);
 
 pixman_color_t qemu_pixman_color(PixelFormat *pf, uint32_t color);
 pixman_image_t *qemu_pixman_glyph_from_vgafont(int height, const uint8_t *font,
