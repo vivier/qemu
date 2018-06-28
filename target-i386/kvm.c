e42d2f4132fa2794ea6915b3dbfa5fc62d2c3eb0
Dr. David Alan Gilbert <dgilbert@redhat.com>
2018-06-28 17:57:10 +0200

    kvm: apic: set APIC base as part of kvm_apic_put
    RH-Author: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-id: <20180628175710.56848-6-dgilbert@redhat.com>
    Patchwork-id: 81148
    O-Subject: [RHEL-7.5.z/RHEL-7.4.z/RHEL-7.3.z qemu-kvm PATCH 5/5] kvm: apic: set APIC base as part of kvm_apic_put
    Bugzilla: 1596299
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>
    
    From: "Dr. David Alan Gilbert" <dgilbert@redhat.com>
    
    RH-Author: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-id: <20180515115634.24469-6-dgilbert@redhat.com>
    Patchwork-id: 80271
    O-Subject: [RHEL-7.6 qemu-kvm PATCH v2 5/5] kvm: apic: set APIC base as part of kvm_apic_put
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>
    RH-Acked-by: Eduardo Habkost <ehabkost@redhat.com>
    
    From: "Dr. David Alan Gilbert" <dgilbert@redhat.com>
    
    The parsing of KVM_SET_LAPIC's input depends on the current value of the
    APIC base MSR---which indeed is stored in APICCommonState---but for historical
    reasons APIC base is set through KVM_SET_SREGS together with cr8 (which is
    really just the APIC TPR) and the actual "special CPU registers".
    
    APIC base must now be set before the actual LAPIC registers, so do that
    in kvm_apic_put.  It will be set again to the same value with KVM_SET_SREGS,
    but that's not a big issue.
    
    This only happens since Linux 4.8, which checks for x2apic mode in
    KVM_SET_LAPIC.  However it's really a QEMU bug; until the recent
    commit 78d6a05 ("x86/lapic: Load LAPIC state at post_load", 2016-09-13)
    QEMU was indeed setting APIC base (via KVM_SET_SREGS) before the other
    LAPIC registers.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit f8d9ccf8d5f9f4b7d364100871c4c7303b546de5)
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>
    (cherry picked from commit 4142f7546da561898f15169f6e8085167601e878)
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- target-i386/kvm.c
+++ target-i386/kvm.c
@@ -1155,6 +1155,14 @@ static int kvm_put_one_msr(X86CPU *cpu, int index, uint64_t value)
     return kvm_vcpu_ioctl(CPU(cpu), KVM_SET_MSRS, &msr_data);
 }
 
+void kvm_put_apicbase(X86CPU *cpu, uint64_t value)
+{
+    int ret;
+
+    ret = kvm_put_one_msr(cpu, MSR_IA32_APICBASE, value);
+    assert(ret == 1);
+}
+
 static int kvm_put_tscdeadline_msr(X86CPU *cpu)
 {
     CPUX86State *env = &cpu->env;
