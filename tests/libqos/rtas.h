119870da72024c6542d2d8224d232fec401224c9
Laurent Vivier <lvivier@redhat.com>
2018-07-18 17:05:01 +0200

    tests: add qrtas_query_irq_number() and qrtas_change_msi()
    These function are needed to manage MSI-X with spapr
    
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>

--- tests/libqos/rtas.h
+++ tests/libqos/rtas.h
@@ -7,6 +7,14 @@
 #define LIBQOS_RTAS_H
 #include "libqos/malloc.h"
 
+/* MSI functions */
+#define RTAS_QUERY_FN           0
+#define RTAS_CHANGE_FN          1
+#define RTAS_RESET_FN           2
+#define RTAS_CHANGE_MSI_FN      3
+#define RTAS_CHANGE_MSIX_FN     4
+#define RTAS_CHANGE_32MSI_FN    5
+
 int qrtas_get_time_of_day(QTestState *qts, QGuestAllocator *alloc,
                           struct tm *tm, uint32_t *ns);
 uint32_t qrtas_ibm_read_pci_config(QTestState *qts, QGuestAllocator *alloc,
@@ -14,4 +22,9 @@ uint32_t qrtas_ibm_read_pci_config(QTestState *qts, QGuestAllocator *alloc,
 int qrtas_ibm_write_pci_config(QTestState *qts, QGuestAllocator *alloc,
                                uint64_t buid, uint32_t addr, uint32_t size,
                                uint32_t val);
+int qrtas_change_msi(QTestState *qts, QGuestAllocator *alloc, uint64_t buid,
+                     uint32_t addr, uint32_t func, uint32_t num_irqs);
+int qrtas_query_irq_number(QTestState *qts, QGuestAllocator *alloc,
+                           uint64_t buid, uint32_t addr, int offset);
+
 #endif /* LIBQOS_RTAS_H */
