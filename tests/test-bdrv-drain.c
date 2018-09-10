751387e28f5006dd063010ff0b0912ff9b44c2fe
John Snow <jsnow@redhat.com>
2018-09-10 18:17:39 +0200

    jobs: change start callback to run callback
    RH-Author: John Snow <jsnow@redhat.com>
    Message-id: <20180910181803.11781-2-jsnow@redhat.com>
    Patchwork-id: 82102
    O-Subject: [RHEL-7.6 qemu-kvm-rhev PATCH 01/25] jobs: change start callback to run callback
    Bugzilla: 1626061
    RH-Acked-by: Max Reitz <mreitz@redhat.com>
    RH-Acked-by: Jeffrey Cody <jcody@redhat.com>
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    
    Presently we codify the entry point for a job as the "start" callback,
    but a more apt name would be "run" to clarify the idea that when this
    function returns we consider the job to have "finished," except for
    any cleanup which occurs in separate callbacks later.
    
    As part of this clarification, change the signature to include an error
    object and a return code. The error ptr is not yet used, and the return
    code while captured, will be overwritten by actions in the job_completed
    function.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Message-id: 20180830015734.19765-2-jsnow@redhat.com
    Reviewed-by: Jeff Cody <jcody@redhat.com>
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    (cherry picked from commit f67432a2019caf05b57a146bf45c1024a5cb608e)
    Signed-off-by: John Snow <jsnow@redhat.com>
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- tests/test-bdrv-drain.c
+++ tests/test-bdrv-drain.c
@@ -501,9 +501,9 @@ static void test_job_completed(Job *job, void *opaque)
     job_completed(job, 0, NULL);
 }
 
-static void coroutine_fn test_job_start(void *opaque)
+static int coroutine_fn test_job_run(Job *job, Error **errp)
 {
-    TestBlockJob *s = opaque;
+    TestBlockJob *s = container_of(job, TestBlockJob, common.job);
 
     job_transition_to_ready(&s->common.job);
     while (!s->should_complete) {
@@ -511,6 +511,7 @@ static void coroutine_fn test_job_start(void *opaque)
     }
 
     job_defer_to_main_loop(&s->common.job, test_job_completed, NULL);
+    return 0;
 }
 
 static void test_job_complete(Job *job, Error **errp)
@@ -525,7 +526,7 @@ BlockJobDriver test_job_driver = {
         .free           = block_job_free,
         .user_resume    = block_job_user_resume,
         .drain          = block_job_drain,
-        .start          = test_job_start,
+        .run            = test_job_run,
         .complete       = test_job_complete,
     },
 };
