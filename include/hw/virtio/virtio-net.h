1a264dee1e8b6ddc93bd205a7670e540774b6c3a
Laurent Vivier <lvivier@redhat.com>
2021-07-23 16:58:45 +0200

    tmp
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>

--- include/hw/virtio/virtio-net.h
+++ include/hw/virtio/virtio-net.h
@@ -208,7 +208,6 @@ struct VirtIONet {
     /* primary failover device is hidden*/
     bool failover_primary_hidden;
     bool failover;
-    DeviceListener primary_listener;
     VirtioNetRssData rss_data;
     struct NetRxPkt *rx_pkt;
     struct EBPFRSSContext ebpf_rss;
@@ -216,5 +215,5 @@ struct VirtIONet {
 
 void virtio_net_set_netclient_name(VirtIONet *n, const char *name,
                                    const char *type);
-
+VirtIONet *virtio_net_pci_get_device(DeviceState *s);
 #endif
