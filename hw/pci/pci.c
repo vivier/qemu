e56291aacac0d701fbc7ebfaf774ca7d168a1f77
Laurent Vivier <lvivier@redhat.com>
2021-08-16 21:54:46 +0200

    failover: hide the PCI device if the virtio-net device is not present
    We can plug either the virtio-net device first or the PCI device
    first.
    
    If we plug the PCI device first, QEMU checks the failover_pair_id
    but doesn't hide the device if the virtio-net device is not present.
    
    This is a problem because if the virtio-net device is not plugged
    and the machine is migrated the VFIO device doesn't prevent anymore
    the migration and the machine is migrated with the VFIO device.
    
    And of course, on destination the guest reboots because VFIO device
    cannot be migrated.
    
    We can fix the problem by hiding the PCI device with a failover_pair_id
    device that is not present. In this case, if the machine is migrated the
    VFIO device is not migrated, and if the virtio-net device is plugged, the
    PCI device is plugged and can be migrated using the failover mechanism.
    
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>

--- hw/pci/pci.c
+++ hw/pci/pci.c
@@ -2287,10 +2287,11 @@ static bool pci_dev_hide_device(DeviceListener *listener,
     d =  qdev_find_recursive(sysbus_get_default(), standby_id);
     if (d == NULL) {
         /*
-         * if the the virtio-net device is not plugged it can be
-         * plugged later, and this device will be added to the failover
+         * PCI device has a pair id, but the virtio-net device is not
+         * plugged: hide it, it will be plugged later when the virtio-net
+         * device will be plugged
          */
-        return false;
+        return true;
     }
 
     hidden = failover_find_primary_qdict(standby_id, NULL, errp);
